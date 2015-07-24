1510504714e48fcf2934b6a693f9a166e76edd37
Jahn Heymann <heymann@nt.upb.de>
2015-07-24 17:48:29 +0200

    Added: Data provider and data fetcher for asynchronous reading of input data

---
+++ tests/nn_test/test_array_data_fetcher.py
@@ -0,0 +1,33 @@
+from nt.nn.data_provider import DataProvider
+from nt.nn.data_fetchers import ArrayDataFetcher
+import numpy
+import numpy.testing
+import unittest
+
+class ArrayDataFetcherTest(unittest.TestCase):
+
+    def setUp(self):
+        self.data = numpy.repeat(numpy.arange(5)[None, :], 10, axis=0)
+        self.data_2 = numpy.repeat(numpy.arange(5,0,-1)[None, :], 10, axis=0)
+        self.fetcher_1 = ArrayDataFetcher('X', self.data)
+        self.fetcher_2 = ArrayDataFetcher('Y', self.data_2)
+        self.dp = DataProvider((self.fetcher_1, self.fetcher_2),
+                               batch_size=2,
+                               max_queue_size=5,
+                               shuffle_data=False)
+
+    def test_setup(self):
+        self.assertEqual(len(self.dp), 5)
+
+    def test_iteration(self):
+        for idx, batch_data in enumerate(self.dp):
+            self.assertTrue('X' in batch_data)
+            self.assertTrue('Y' in batch_data)
+            numpy.testing.assert_equal(batch_data['X'],
+                                       self.data[(idx*2, idx*2+1), ...])
+            numpy.testing.assert_equal(batch_data['Y'],
+                                       self.data_2[(idx*2, idx*2+1), ...])
+
+    def test_reset(self):
+        self.test_iteration()
+        self.test_iteration()
---
+++ tests/nn_test/test_data_provider.py
@@ -0,0 +1,57 @@
+from nt.nn.data_provider import DataProvider
+from nt.nn.data_fetchers.template import DataFetcher
+import numpy
+import numpy.testing
+import unittest
+
+class IdentityFetcher(DataFetcher):
+
+    def __init__(self, name, len=10):
+        DataFetcher.__init__(self, name)
+        self.len = len
+
+    def __len__(self):
+        return self.len
+
+    def get_data_for_idxs(self, idxs):
+        return numpy.asarray(list(idxs))
+
+
+class DataProviderFetcher(unittest.TestCase):
+
+    def setUp(self):
+        self.fetcher_1 = IdentityFetcher('X')
+        self.fetcher_2 = IdentityFetcher('Y')
+        self.dp = DataProvider((self.fetcher_1, self.fetcher_2),
+                               batch_size=2,
+                               max_queue_size=5,
+                               shuffle_data=False)
+
+    def test_setup(self):
+        self.assertEqual(len(self.dp), 5)
+
+    def test_iteration(self):
+        for idx, batch_data in enumerate(self.dp):
+            self.assertTrue('X' in batch_data)
+            self.assertTrue('Y' in batch_data)
+            numpy.testing.assert_equal(batch_data['X'],
+                                       numpy.asarray([idx*2, idx*2+1]))
+            numpy.testing.assert_equal(batch_data['Y'],
+                                       numpy.asarray([idx*2, idx*2+1]))
+
+    def test_reset(self):
+        self.test_iteration()
+        self.test_iteration()
+
+    def test_random(self):
+        self.dp.shuffle_data = True
+        self.assertRaises(AssertionError, self.test_iteration)
+
+    def test_different_length(self):
+        self.fetcher_2.len = 11
+        def create_dp():
+            self.dp = DataProvider((self.fetcher_1, self.fetcher_2),
+                               batch_size=2,
+                               max_queue_size=5,
+                               shuffle_data=False)
+        self.assertRaises(EnvironmentError, create_dp)
