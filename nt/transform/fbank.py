be4529cba6c8c4a79104bdf81ef799175e59f9b0
Oliver Walter <walter@nt.uni-paderborn.de>
2015-07-27 16:38:11 +0200

    ćleanup parameter names - should be same in all functions

--- nt/transform/fbank.py
+++ nt/transform/fbank.py
@@ -7,7 +7,7 @@ import nt.transform.filter as filter
 import nt.transform.stft as stft
 import scipy.signal
 
-def fbank(signal, sample_rate=16000, window_length=400, stft_shift=160,
+def fbank(time_signal, sample_rate=16000, window_length=400, stft_shift=160,
           number_of_filters=26, stft_size=512, lowest_frequency=0,
           highest_frequency=None, preemphasis=0.97,
           window=scipy.signal.hamming):
@@ -17,7 +17,7 @@ def fbank(signal, sample_rate=16000, window_length=400, stft_shift=160,
     Source: https://github.com/jameslyons/python_speech_features
     Tutorial: http://www.practicalcryptography.com/miscellaneous/machine-learning/guide-mel-frequency-cepstral-coefficients-mfccs/ # noqa
 
-    :param signal: the audio signal from which to compute features.
+    :param time_signal: the audio signal from which to compute features.
         Should be an N*1 array
     :param sample_rate: the samplerate of the signal we are working with.
     :param window_length: the length of the analysis window in samples.
@@ -36,13 +36,13 @@ def fbank(signal, sample_rate=16000, window_length=400, stft_shift=160,
     :returns: Mel filterbank features.
     """
     highest_frequency = highest_frequency or sample_rate/2
-    signal = filter.offcomp(signal)
-    signal = filter.preemphasis(signal, preemphasis)
+    time_signal = filter.offcomp(time_signal)
+    time_signal = filter.preemphasis(time_signal, preemphasis)
 
-    stft_signal = stft.stft(signal, size=stft_size, shift=stft_shift,
-                      window=window, window_length=window_length)
+    stft_signal = stft.stft(time_signal, size=stft_size, shift=stft_shift,
+                      window=window, window_length=window_length, fading=False)
 
-    spectrogram = stft.stft_to_spectrogram(stft_signal)
+    spectrogram = stft.stft_to_spectrogram(stft_signal)/stft_size
 
     filterbanks = get_filterbanks(number_of_filters, stft_size, sample_rate,
                                   lowest_frequency, highest_frequency)
@@ -56,7 +56,7 @@ def fbank(signal, sample_rate=16000, window_length=400, stft_shift=160,
     return feature
 
 
-def get_filterbanks(number_of_filters=20, nfft=1024, samplerate=16000,
+def get_filterbanks(number_of_filters=20, nfft=1024, sample_rate=16000,
                     lowfreq=0, highfreq=None):
     """Compute a Mel-filterbank. The filters are stored in the rows, the columns
     correspond to fft bins. The filters are returned as an array of size
@@ -66,15 +66,15 @@ def get_filterbanks(number_of_filters=20, nfft=1024, samplerate=16000,
 
     :param nfilt: the number of filters in the filterbank, default 20.
     :param nfft: the FFT size. Default is 1024.
-    :param samplerate: the samplerate of the signal we are working with.
+    :param sample_rate: the samplerate of the signal we are working with.
         Affects mel spacing.
     :param lowfreq: lowest band edge of mel filters, default 0 Hz
     :param highfreq: highest band edge of mel filters, default samplerate/2
     :returns: A numpy array of size nfilt * (nfft/2 + 1) containing filterbank.
         Each row holds 1 filter.
     """
-    highfreq = highfreq or samplerate/2
-    assert highfreq <= samplerate/2, "highfreq is greater than samplerate/2"
+    highfreq = highfreq or sample_rate/2
+    assert highfreq <= sample_rate/2, "highfreq is greater than samplerate/2"
 
     # compute points evenly spaced in mels
     lowmel = hz2mel(lowfreq)
@@ -82,7 +82,7 @@ def get_filterbanks(number_of_filters=20, nfft=1024, samplerate=16000,
     melpoints = numpy.linspace(lowmel, highmel, number_of_filters+2)
     # our points are in Hz, but we use fft bins, so we have to convert
     #  from Hz to fft bin number
-    bin = numpy.floor((nfft+1)*mel2hz(melpoints)/samplerate)
+    bin = numpy.floor((nfft+1)*mel2hz(melpoints)/sample_rate)
 
     fbank = numpy.zeros([number_of_filters, nfft/2+1])
     for j in range(0, number_of_filters):
