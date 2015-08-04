4932db310b6eb4db6b75ce7791e560d444d0918b
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2015-08-04 16:23:45 +0200

    add wrapper for numpy.testing: nt.testing   add funktions:   *assert_array_greater   *assert_array_greater_equal   *assert_array_less_equal   *assert_isreal

---
+++ nt/testing/__init__.py
@@ -0,0 +1,5 @@
+"""
+This module simply wraps all numpy.testing functions and provides additional assertions for our cases.
+"""
+from nt.testing.module_asserts import *
+from numpy.testing import *
