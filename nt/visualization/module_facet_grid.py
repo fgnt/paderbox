e4c40161a03e4f975c1bf9281af9dbdf458c0c43
Jahn Heymann <heymann@nt.upb.de>
2015-08-06 17:55:02 +0200

    Allow arguments for plotting functions

--- nt/visualization/module_facet_grid.py
+++ nt/visualization/module_facet_grid.py
@@ -4,13 +4,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def facet_grid(data_list, function_list, colwrap=2, scale=1):
+def facet_grid(data_list, function_list, kwargs_list=(), colwrap=2, scale=1):
     assert len(function_list) == len(data_list) or \
            len(function_list) == 1 or \
            len(data_list) == 1
+    assert len(kwargs_list) == 0 or len(kwargs_list) == len(data_list)
 
     number_of_plots = max(len(function_list), len(data_list))
     number_of_rows = int(np.ceil(number_of_plots / colwrap))
+
+    if len(kwargs_list) == 0:
+        kwargs_list = len(data_list) * [{}]
 
     with sns.axes_style("darkgrid"):
         figure, axis = plt.subplots(number_of_rows, colwrap)
@@ -25,7 +29,8 @@ def facet_grid(data_list, function_list, colwrap=2, scale=1):
 
         for index in range(len(data_list)):
             function_list[index](ax=axis.flatten()[index],
-                                 signal=data_list[index])
+                                 signal=data_list[index],
+                                 **kwargs_list[index])
 
         for index in range(number_of_plots, number_of_rows * colwrap):
             axis.flatten()[index].axis('off')
