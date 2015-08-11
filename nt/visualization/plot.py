9b57b4165aab055eda13dfea07d28e2b80366569
Jahn Heymann <heymann@nt.upb.de>
2015-08-11 16:54:37 +0200

    Refactoring Plot CTC probabilities instead of unnormalized ones

--- nt/visualization/plot.py
+++ nt/visualization/plot.py
@@ -1,12 +1,11 @@
 import seaborn as sns
 import numpy as np
-import matplotlib as mpl
-import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 
 import nt.transform
 
 COLORMAP = sns.diverging_palette(220, 20, n=7, as_cmap=True)
+
 
 def time_series(signal, ax):
     """
@@ -56,6 +55,7 @@ def spectrogram(signal, limits=None, ax=None):
         ax.set_ylabel('Frequency bin index')
         ax.grid(False)
 
+
 def stft(signal, limits=None, ax=None):
     """
     Plots a spectrogram from an stft signal as input. This is a wrapper of the
@@ -66,6 +66,7 @@ def stft(signal, limits=None, ax=None):
     :return: None
     """
     spectrogram(nt.transform.stft_to_spectrogram(signal), limits=limits, ax=ax)
+
 
 def mask(signal, ax=None, **kwargs):
     """
@@ -88,6 +89,7 @@ def mask(signal, ax=None, **kwargs):
         ax.set_ylabel('Frequency bin index')
         ax.grid(False)
 
+
 def plot_ctc_decode(decode, label_handler, ax=None):
     """ Plot a ctc decode
 
@@ -96,14 +98,87 @@ def plot_ctc_decode(decode, label_handler, ax=None):
     :param ax: Optional figure axes to use with facet_grid()
     :return:
     """
+    if decode.ndim == 3:
+        net_out = decode[:, 0, :]
+    else:
+        net_out = decode
+    net_out = net_out - np.amax(net_out)
+    net_out_e = np.exp(net_out)
+    net_out = net_out_e / \
+              (np.sum(net_out_e, axis=1, keepdims=True) + 1e-20)
 
     with sns.axes_style("darkgrid"):
         if ax is None:
             figure, ax = plt.subplots(1, 1)
-    for char in range(decode.shape[2]):
-        _ = ax.plot(decode[:, 0, char], label=label_handler.int_to_label[char])
-        plt.legend(loc='lower center',
-                   ncol=decode.shape[2]//3,
-                   bbox_to_anchor=[0.5, -0.35])
-    ax.set_xlabel('Time frame index')
-    ax.set_ylabel('Unnormalized Propability')
+        for char in range(decode.shape[2]):
+            _ = ax.plot(net_out[:, char],
+                        label=label_handler.int_to_label[char])
+            plt.legend(loc='lower center',
+                       ncol=decode.shape[2] // 3,
+                       bbox_to_anchor=[0.5, -0.35])
+        ax.set_xlabel('Time frame index')
+        ax.set_ylabel('Propability')
+
+
+def plot_nn_current_loss(status, ax=None):
+    with sns.axes_style("darkgrid"):
+        if ax is None:
+            figure, ax = plt.subplots(1, 1)
+        plot = False
+
+        if len(status.loss_current_batch_training) > 1:
+            ax.plot(status.loss_current_batch_training,
+                    label='training')
+            plot = True
+        if len(status.loss_current_batch_cv) > 1:
+            ax.plot(status.loss_current_batch_cv,
+                    label='cross-validation')
+            plot = True
+        if plot:
+            ax.set_xlabel('Iterations')
+            ax.set_title('Batch loss')
+            plt.legend()
+
+
+def plot_nn_current_loss_distribution(status, ax=None):
+    with sns.axes_style("darkgrid"):
+        if ax is None:
+            figure, ax = plt.subplots(1, 1)
+        plot = False
+
+        if len(status.loss_current_batch_training) > 10:
+            sns.distplot(status.loss_current_batch_training,
+                         label='training', ax=ax)
+            plot = True
+        if len(status.loss_current_batch_cv) > 10:
+            sns.distplot(status.loss_current_batch_cv,
+                         label='cross-validation', ax=ax)
+            plot = True
+        if plot:
+            ax.set_xlabel('Loss')
+            ax.set_title('Probability')
+            plt.legend()
+
+
+def plot_nn_current_timings_distribution(status, ax=None):
+    with sns.axes_style("darkgrid"):
+        if ax is None:
+            figure, ax = plt.subplots(1, 1)
+        plot = False
+
+        if len(status.cur_time_forward) > 10:
+            sns.distplot(status.cur_time_forward,
+                         label='training forward', ax=ax)
+            plot = True
+        if len(status.cur_time_backprop) > 10:
+            sns.distplot(status.cur_time_backprop,
+                         label='training backprop', ax=ax)
+            plot = True
+        if len(status.cur_time_cv) > 10:
+            sns.distplot(status.cur_time_cv,
+                         label='cross-validation', ax=ax)
+            plot = True
+        if plot:
+            ax.set_xlabel('Time [ms]')
+            ax.set_title('Probability')
+            plt.legend()
