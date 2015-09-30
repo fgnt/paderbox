e57b89facce870c275c61b317b828b09f2f19699
Janek Ebbers <ebbers@nt.upb.de>
2015-09-30 18:21:59 +0200

    improved profiling decorators and related example

--- nt/utils/profiling.py
+++ nt/utils/profiling.py
@@ -3,28 +3,39 @@ from line_profiler import LineProfiler
 import types
 import cProfile
 import time
+from pycallgraph import PyCallGraph
+from pycallgraph.output import GraphvizOutput
 
-
-def timefunc(f):
-    def f_timer(*args, **kwargs):
-        start = time.time()
-        result = f(*args, **kwargs)
-        end = time.time()
-        print(f.__name__, 'took', end - start, 'time')
-        return result
-    return f_timer
-
-def do_cprofile(func):
+def timefunc(func):
     def profiled_func(*args, **kwargs):
-        profile = cProfile.Profile()
-        try:
-            profile.enable()
-            result = func(*args, **kwargs)
-            profile.disable()
-            return result
-        finally:
-            profile.print_stats()
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(func.__name__, 'took', end - start, 'time')
+        return result
     return profiled_func
+
+
+def do_cprofile(sort_type='tottime'):
+    def inner(func):
+        def profiled_func(*args, **kwargs):
+            profile = cProfile.Profile()
+            try:
+                profile.enable()
+                result = func(*args, **kwargs)
+                profile.disable()
+                return result
+            finally:
+                profile.print_stats(sort=sort_type)
+        return profiled_func
+    return inner
+
+def do_graphprofile(func):
+    def profiled_func(*args, **kwargs):
+        with PyCallGraph(output=GraphvizOutput()):
+            return func(*args, **kwargs)
+    return profiled_func
+
 
 def do_lineprofile(modules_under_test=list()):
     def inner(func):
@@ -49,18 +60,28 @@ def do_lineprofile(modules_under_test=list()):
         return profiled_func
     return inner
 
+
 # ========Example=========
 
 if __name__ == "__main__":
 
-    def get_number():
-        for x in range(1000):
-            yield x
+    def fibonacci(n):
+        value_old = 1
+        value = 1
+        for x in range(n):
+            if x>1:
+                temp = value
+                value = value + value_old
+                value_old = temp
+            yield value
 
-    @do_lineprofile(modules_under_test=[get_number])
-    @do_cprofile
-    def some_calculation():
-        for x in get_number():
-            i = 2*x
-        return 'some result!'
-    result = some_calculation()
+    #@do_cprofile("ncall")
+    @do_graphprofile
+    #@do_lineprofile([fibonacci])
+    #@timefunc
+    def example_func():
+        fib = 0
+        for value in fibonacci(100):
+            fib = value
+        return fib
+    print(example_func())
