08e05f56834dc0649e547aa4a7c7c3231b920a24
Jahn Heymann <heymann@nt.upb.de>
2015-10-02 08:47:38 +0200

    Updated PESQ scoring to support threaded scoring

--- tests/evaluation/test_pesq.py
+++ tests/evaluation/test_pesq.py
@@ -1,45 +1,29 @@
 import unittest
-from nt.evaluation.pesq import pesq
-from scipy.io import wavfile
-import wave, struct
+from nt.evaluation.pesq import pesq, threaded_pesq
+import numpy
+import numpy.testing as nptest
 from nt.io.audioread import audioread
-
-
-
 
 class TestPESQ(unittest.TestCase):
 
     def setUp(self):
         self.ref = 'data/speech.wav'
         self.deg = 'data/speech_bab_0dB.wav'
-        # rate, refer = wavfile.read('data/speech.wav')
-        # waveFile = wave.open('data/speech.wav', 'r')
-        # length = waveFile.getnframes()
-        # for i in range(0,length):
-        #     waveData = waveFile.readframes(1)
-        #     data = struct.unpack("<h", waveData)
-        #
-        # self.rate = rate
-        # print('this is original data')
-        # print(refer)
-        # self.refer = refer
         self.refer = audioread(self.ref)
         self.rate = 16000
 
     def test_wb_scores(self):
         scores = pesq(self.ref, self.deg, 'wb', self.rate)
-        self.assertEqual(scores, (1.083, 0))
-        print(scores)
+        nptest.assert_equal(scores, numpy.asarray([[0, 1.083]]))
 
     def test_nb_scores(self):
         scores = pesq(self.ref, self.deg, 'nb', self.rate)
-        self.assertEqual(scores, (1.607, 1.969))
-        print(scores)
+        nptest.assert_equal(scores, numpy.asarray([[1.969, 1.607]]))
 
-r = TestPESQ()
-r.setUp()
+    def test_wb_scores_thread(self):
+        scores = threaded_pesq(3*[self.ref], 3*[self.deg], 'wb', self.rate)
+        nptest.assert_equal(scores, numpy.asarray(3*[[0, 1.083]]))
 
-print('here goes the nb')
-r.test_nb_scores()
-#print('here goes the wb')
-#r.test_wb_scores()
+    def test_nb_scores_thread(self):
+        scores = threaded_pesq(3*[self.ref], 3*[self.deg], 'nb', self.rate)
+        nptest.assert_equal(scores, numpy.asarray(3*[[1.969, 1.607]]))
