87fd8464f64fc72937c6418b6329a1d1bfff518d
Jahn Heymann <heymann@nt.upb.de>
2015-10-22 15:54:32 +0200

    Softmax function

--- nt/utils/math_ops.py
+++ nt/utils/math_ops.py
@@ -1,6 +1,4 @@
-
 import numpy as np
-
 
 def normalize_vector_to_unit_length(vector):
     """
@@ -16,3 +14,16 @@ def normalize_vector_to_unit_length(vector):
 
 def vector_H_vector(x, y):
     return np.einsum('...a,...a->...', x.conj(), y)
+
+
+def softmax(x, feature_axis=-1):
+    """ Calculates the softmax activation
+
+    :param x: Input signal
+    :param feature_axis: Dimension holding the features to apply softmax on
+    :return: Softmax features
+    """
+    net_out_e = x - x.max(axis=feature_axis, keepdims=True)
+    np.exp(net_out_e, out=net_out_e)
+    net_out_e /= net_out_e.sum(axis=feature_axis, keepdims=True)
+    return net_out_e
