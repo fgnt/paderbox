03582947bf29e5da17df537ccea88fc7d83dd048
Jahn Heymann <heymann@nt.upb.de>
2015-11-02 09:10:08 +0100

    Add hybrid label handler

--- nt/utils/transcription_handling.py
+++ nt/utils/transcription_handling.py
@@ -80,6 +80,94 @@ class WordLabelHandler():
     def __len__(self):
         return len(self.label_to_int)
 
+
+class HybridLabelHandler():
+    """ Handles transforming from words/chars to integers and vice versa
+
+        This Handler creates a unique integer for each word which appears at
+        least ``min_count`` times in the corpus. Other words are transformed to
+        their underlying character sequence and each character has its unique
+        integer.
+
+    """
+
+    def __init__(self, transcription_list, add_blank=True, min_count=20):
+        self.label_to_int = dict()
+        self.int_to_label = dict()
+        if add_blank:
+            self.label_to_int['BLANK'] = 0
+            self.int_to_label[0] = 'BLANK'
+        self.label_to_int['<UNK>'] = len(self.label_to_int)
+        self.int_to_label[len(self.int_to_label)] = '<UNK>'
+        self.character_idxs = list()
+        word_count = dict()
+        for transcription in transcription_list:
+            for word in transcription.split():
+                try:
+                    word_count[word] += 1
+                except KeyError:
+                    word_count[word] = 1
+        for word, count in word_count.items():
+            if count > min_count:
+                if not word in self.label_to_int:
+                    number = len(self.label_to_int)
+                    self.label_to_int[word] = number
+                    self.int_to_label[number] = word
+            else:
+                for c in word:
+                    if c not in self.label_to_int:
+                        number = len(self.label_to_int)
+                        self.label_to_int[c] = number
+                        self.int_to_label[number] = c
+                        self.character_idxs.append(number)
+
+    def label_seq_to_int_arr(self, label_seq):
+        int_arr = list()
+        for word in label_seq.split():
+            try:
+                int_arr.append(self.label_to_int[word])
+            except KeyError:
+                # We have an unknown word and now return its spelling
+                for c in word:
+                    try:
+                        int_arr.append(self.label_to_int[c])
+                    except KeyError:
+                        int_arr.append(self.label_to_int['<UNK>'])
+        return numpy.asarray(int_arr, dtype=numpy.int32)
+
+    def int_arr_to_label_seq(self, int_arr):
+        char_state = False
+        ret_str = ''
+        for idx in int_arr:
+            if idx > len(self.label_to_int):
+                raise ValueError('No label for index {}'.format(idx))
+            if not idx in self.character_idxs:
+                if char_state:
+                    ret_str += '</w> ' + self.int_to_label[idx]
+                    char_state = False
+                else:
+                    if len(ret_str) > 0:
+                        ret_str += ' ' + self.int_to_label[idx]
+                    else:
+                        ret_str += self.int_to_label[idx]
+            else:
+                if char_state:
+                    ret_str += self.int_to_label[idx]
+                else:
+                    if len(ret_str) > 0:
+                        ret_str += ' <w>' + self.int_to_label[idx]
+                    else:
+                        ret_str += '<w>' + self.int_to_label[idx]
+                    char_state = True
+        return ret_str
+
+    def print_mapping(self):
+        for char, i in self.label_to_int.items():
+            print('{} -> {}'.format(char, i))
+
+    def __len__(self):
+        return len(self.label_to_int)
+
 def argmax_ctc_decode(int_arr, label_handler):
     """ Decodes a ctc sequence
 
@@ -99,5 +187,6 @@ def argmax_ctc_decode(int_arr, label_handler):
         elif idx == 0:
             decode[idx_dec] = n
             idx_dec += 1
-    chars = [label_handler.int_to_label[c] for c in decode if c != 0]
-    return ''.join(chars)
+    idx_seq = [c for c in decode if c != 0]
+    sequence = label_handler.int_arr_to_label_seq(idx_seq)
+    return sequence
