cf1b90107fc29229ea7f48ddbe24c0173b0450a6
Wilhelm Kirchgaessner <wilhelmk@mail.uni-paderborn.de>
2015-11-03 15:32:08 +0100

    added Testcase comparing similarity between given reverberation time and calculated time using schroeders method from matlab

--- tests/reverb_tests/test_reverb_utils.py
+++ tests/reverb_tests/test_reverb_utils.py
@@ -4,6 +4,7 @@ from nt.utils.matlab import Mlab, matlab_test
 import nt.testing as tc
 import numpy.testing as nptest
 import nt.reverb.reverb_utils as rirUtils
+from nt.utils.profiling import *
 
 # Uncomment, if you want to test Matlab functions.
 matlab_test = unittest.skipUnless(True, 'matlab-test')
@@ -106,15 +107,71 @@ class TestReverbUtils(unittest.TestCase):
         expected = distance / 343
         tc.assert_allclose(actual, expected, atol=1e-4)
 
-    @unittest.skip("")
-    @matlab_test
-    def test_compareTranVuExpectedT60WithCalculatedUsingSchroederMethod(self):
-        pass
 
-    @unittest.skip("")
+    @matlab_test
+    def test_compareTranVuExpectedT60WithCalculatedUsingSchroederMethodFromMatlab(self):
+        """
+        Compare minimal time-delay of RIR calculated by TranVu's algorithm
+        with expected propagation-time by given distance and soundvelocity.
+
+        Similarity ranges between 0.1 and 0.2 difference depending on given
+        T60.
+        """
+        number_of_sources = 1
+        number_of_sensors = 1
+        T60 = 0.2
+
+        sources, mics = rirUtils.generateRandomSourcesAndSensors(
+            self.room_dimensions,
+            number_of_sources,
+            number_of_sensors
+        )
+        # By using TranVu the first index of returned RIR equals time-index -128
+        fixedshift = 128
+
+        rir = rirUtils.generate_RIR(self.room_dimensions,
+                                    sources,
+                                    mics,
+                                    self.sample_rate,
+                                    self.filter_length,
+                                    T60)
+
+        if number_of_sources == 1:
+            rir = numpy.reshape(rir,(self.filter_length,1))
+            assert rir.shape == (self.filter_length,1)
+
+        matlab_session = self.matlab_session
+        matlab_session.run_code("sampleRate = {0};".format(self.sample_rate))
+        matlab_session.run_code("fixedShift = {0};".format(fixedshift))
+        matlab_session.run_code("rir = zeros({0},{1},{2})".format(
+            self.filter_length,number_of_sensors,number_of_sources))
+        codeblock = ""
+        for m in rir:
+            codeblock += "{0};".format(m)
+        codeblock = codeblock[:-1] # omit last comma
+        matlab_session.run_code("rir = ["+codeblock+"];")
+        matlabRIR = matlab_session.get_variable('rir')
+        matlab_session.run_code("actual = RT_schroeder(rir(fixedShift+1:end)',sampleRate);")
+        actualT60 = matlab_session.get_variable('actual')
+
+        tc.assert_allclose(matlabRIR, rir, atol=1e-4)
+        tc.assert_allclose(actualT60, T60, atol=0.14)
+
     @matlab_test
     def test_compareDirectivityWithExpectedUsingTranVu(self):
-        pass
+        """
+        Compare signal-power of RIR calculated by TranVu's algorithm
+        from different directivities with expected characteristic
+        """
+        SensorOrientationAngle = 0
+        fixedShift = 128
+        T60 = 0
+        deltaAngle = numpy.pi/16
+        #azimuthAngle =
+        # todo: Testcase weiterschreiben.
+
+
+
 
     @unittest.skip("")
     @matlab_test
