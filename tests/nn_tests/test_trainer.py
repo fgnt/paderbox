6b9464e95df7060c2046f851f34a3e9b5efae9e1
Jahn Heymann <heymann@nt.upb.de>
2015-11-20 15:26:16 +0100

    WIP: Trainer + Tests

--- tests/nn_tests/test_trainer.py
+++ tests/nn_tests/test_trainer.py
@@ -1,24 +1,48 @@
 import unittest
 from nt.nn import *
 import numpy as np
-from chainer import FunctionSet
-import chainer.functions as F
 from chainer.optimizers import SGD
 import os
 import time
 import numpy.testing as nptest
+from chainer.link import Chain
+from chainer.functions import mean_squared_error
+from chainer.links import Linear
+from chainer.optimizer import GradientClipping
+import tempfile
 
 B = 10
 A = 5
 
+class DummyNetwork(Chain):
+
+    def __init__(self):
+        super(DummyNetwork, self).__init__(
+            l1 = Linear(5, 3),
+            l2 = Linear(3, 5)
+        )
+
+    def forward(self, **kwargs):
+        net_in = kwargs.pop('i')
+        target = kwargs.pop('t')
+
+        h = self.l1(net_in)
+        h = self.l2(h)
+        return mean_squared_error(h, target)
+
+    def forward_train(self, **kwargs):
+        return self.forward(**kwargs)
+
+    def forward_cv(self, **kwargs):
+        return self.forward(**kwargs)
+
+
 class TrainerTest(unittest.TestCase):
 
     def setUp(self):
-        self.nn = NeuralNetwork(inputs=('i', 't'), outputs=('h0', 'y', 'l'))
         self.input = np.random.uniform(-1, 1, (B, A)).astype(np.float32)
         self.target = self.input.copy()
-        self.nn.layers = FunctionSet(l1=F.Linear(5, 3),
-                                     l2=F.Linear(3, 5))
+        self.nn = DummyNetwork()
         x_fetcher = ArrayDataFetcher('i', self.input)
         t_fetcher = ArrayDataFetcher('t', self.target)
         x_cv_fetcher = ArrayDataFetcher('i', self.input)
@@ -26,48 +50,36 @@ class TrainerTest(unittest.TestCase):
         self.tr_provider = DataProvider((x_fetcher, t_fetcher), batch_size=2)
         self.cv_provider = DataProvider((x_cv_fetcher, t_cv_fetcher),
                                         batch_size=2)
-        self.optimizer = SGD(1e-5)
-        self.optimizer.setup(self.nn.layers)
+        hooks = [GradientClipping(1)]
         self.trainer = Trainer(self.nn,
-                               forward_fcn=self._forward,
+                               forward_fcn_tr=self.nn.forward_train,
+                               forward_fcn_cv=self.nn.forward_cv,
                                data_provider_tr=self.tr_provider,
                                data_provider_cv=self.cv_provider,
-                               optimizer=self.optimizer,
-                               loss_name='l',
+                               optimizer=SGD(),
                                description='unittest',
-                               data_dir='test_tmp',
-                               epochs=50,
-                               logging_list=['h0'],
-                               grad_clip=5,
-                               use_gpu=False)
-
-    def _forward(self, nn):
-        h0 = F.sigmoid(nn.layers.l1(nn.inputs.i))
-        y = nn.layers.l2(h0)
-        nn.outputs.l = F.mean_squared_error(y, nn.inputs.t)
-        nn.outputs.h0 = h0
-        nn.outputs.y = y
+                               data_dir=tempfile.mkdtemp(),
+                               epochs=100,
+                               use_gpu=False,
+                               optimizer_hooks=hooks)
 
     def test_init(self):
         self.assertTrue(os.path.exists(self.trainer.data_dir))
 
     def test_training_step(self):
-        self.nn.mode = 'Train'
-        self.trainer.optimizer.setup(self.nn.layers)
+        self.trainer.optimizer.setup(self.nn)
         batch = self.tr_provider.test_run()
         self.trainer._train_forward_batch(batch)
-        self.assertGreater(self.nn.outputs.l.num, 0)
-        self.assertEqual(self.nn.outputs.y.num.shape, (2, 5))
+        self.assertGreater(self.trainer.current_loss.num, 0)
         self.trainer._reset_gradients()
-        nptest.assert_equal(self.nn.layers.l2.gW, np.zeros((5, 3)))
+        nptest.assert_equal(self.nn.l2.W.grad, np.zeros((5, 3)))
         self.trainer._backprop()
-        self.assertFalse(np.sum(self.nn.layers.l2.gW) == 0)
-        W2_before = self.nn.layers.l2.W.copy()
+        W2_before = self.nn.l2.W.data.copy()
         self.trainer._update_parameters()
         self.assertRaises(
-            AssertionError, nptest.assert_equal, W2_before, self.nn.layers.l2.W)
+            AssertionError, nptest.assert_equal, W2_before, self.nn.l2.W.data)
         self.trainer._reset_gradients()
-        nptest.assert_equal(self.nn.layers.l2.gW, np.zeros((5, 3)))
+        nptest.assert_equal(self.nn.l2.W.grad, np.zeros((5, 3)))
 
     def _test_run_stop(self, use_gpu=False):
         self.trainer.run_in_thread = True
@@ -90,9 +102,8 @@ class TrainerTest(unittest.TestCase):
         if use_gpu:
             self.trainer.use_gpu = True
         self.trainer.start_training()
-        request = ['l', 'h0', 'y']
         time.sleep(2)
-        responds = self.trainer.get_status(request)
+        responds = self.trainer.get_status()
         self.assertEqual(responds.description, 'unittest')
         for var in responds.__dict__.keys():
             if isinstance(var, float):
