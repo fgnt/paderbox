05299d0ec7f0e722c8459141521057c03606e05b
Lukas Drude <mail@lukas-drude.de>
2016-01-07 17:48:24 +0100

    New

--- nt/utils/math_ops.py
+++ nt/utils/math_ops.py
@@ -27,3 +27,27 @@ def softmax(x, feature_axis=-1):
     np.exp(net_out_e, out=net_out_e)
     net_out_e /= net_out_e.sum(axis=feature_axis, keepdims=True)
     return net_out_e
+
+
+def covariance(x, mask=None):
+    """ Calculate the covariance of a zero mean signal.
+
+    The leading dimensions are independent and can be arbitrary.
+
+    :param x: Signal with dimensions ... times F times M times T.
+    :param mask: Mask with dimensions ... times F times T.
+    :return: Covariance matrices with dimensions ... times F times M times M.
+    """
+
+    if mask is None:
+        psd = np.einsum('...dt,...et->...de', x, x.conj())
+        psd /= x.shape[-1]
+    else:
+        assert x.ndim == mask.ndim + 1, 'Wrong total number of dimensions.'
+        assert x.shape[-1] == mask.shape[-1], 'Check dimension for summation.'
+        mask = np.expand_dims(mask, -2)
+        psd = np.einsum('...dt,...et->...de', mask * x, x.conj())
+        normalization = np.maximum(np.sum(mask, axis=-1, keepdims=True), 1e-10)
+        psd /= normalization
+
+    return psd
---
+++ tests/utils_test/test_covariance.py
@@ -0,0 +1,62 @@
+import unittest
+import numpy as np
+import nt.testing as tc
+from nt.utils.math_ops import covariance
+
+
+def rand(*shape, data_type=np.float64):
+    if not shape:
+        shape = (1,)
+    elif isinstance(shape[0], tuple):
+        shape = shape[0]
+
+    def uniform(data_type_local):
+        return np.random.uniform(-1, 1, shape).astype(data_type_local)
+
+    if data_type in (np.float32, np.float64):
+        return uniform(data_type)
+    elif data_type is np.complex64:
+        return uniform(np.float32) + 1j * uniform(np.float32)
+    elif data_type is np.complex128:
+        return uniform(np.float64) + 1j * uniform(np.float64)
+
+
+class TestCovariance(unittest.TestCase):
+    def generate_data(self, x_shape, mask_shape):
+        x = rand(x_shape, data_type=np.complex128)
+        mask = np.random.uniform(0, 1, mask_shape)
+        mask = mask / np.sum(mask, axis=0, keepdims=True)
+        return x, mask
+
+    def test_PSD_without_mask(self):
+        x = rand(3, 4)
+        psd = covariance(x)
+        tc.assert_equal(psd.shape, (3, 3))
+        tc.assert_positive_semidefinite(psd)
+
+    def test_PSD_with_mask(self):
+        x = rand(3, 4)
+        mask = np.random.uniform(0, 1, (4,))
+        psd = covariance(x, mask)
+        tc.assert_equal(psd.shape, (3, 3))
+        tc.assert_positive_semidefinite(psd)
+
+    def test_PSD_with_mask_with_source(self):
+        x = rand(3, 4)
+        mask = np.random.uniform(0, 1, (2, 4))
+        psd = covariance(x[np.newaxis, ...], mask)
+        tc.assert_equal(psd.shape, (2, 3, 3))
+        tc.assert_positive_semidefinite(psd)
+
+    def test_PSD_with_mask_independent_dim(self):
+        x = rand(2, 3, 4)
+        mask = np.random.uniform(0, 1, (2, 4,))
+        psd = covariance(x, mask)
+        tc.assert_equal(psd.shape, (2, 3, 3))
+        tc.assert_positive_semidefinite(psd)
+
+    def test_PSD_without_mask_independent_dim(self):
+        x = rand(1, 2, 3, 4)
+        psd = covariance(x)
+        tc.assert_equal(psd.shape, (1, 2, 3, 3))
+        tc.assert_positive_semidefinite(psd)
