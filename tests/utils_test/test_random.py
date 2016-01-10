164a9749a5f9560d60fe9f58839fcfa720545d14
Lukas Drude <mail@lukas-drude.de>
2016-01-10 11:39:23 +0100

    Moved random uniform function which has already been used in many test cases to its own spot. It also has random herimitian matrix generation.

---
+++ tests/utils_test/test_random.py
@@ -0,0 +1,22 @@
+import unittest
+import numpy as np
+import nt.testing as tc
+import nt.utils.random as random
+
+
+class TestRandom(unittest.TestCase):
+    def test_uniform_scalar(self):
+        scalar = random.uniform()
+        tc.assert_equal(scalar.shape, (1,))
+
+    def test_hermitian_2D(self):
+        shape = (2, 2)
+        matrix = random.hermitian(*shape)
+        tc.assert_equal(matrix.shape, shape)
+        tc.assert_equal(matrix, np.swapaxes(matrix, -1, -2).conj())
+
+    def test_hermitian_many_dimensions(self):
+        shape = (3, 4, 5, 2, 2)
+        matrix = random.hermitian(*shape)
+        tc.assert_equal(matrix.shape, shape)
+        tc.assert_equal(matrix, np.swapaxes(matrix, -1, -2).conj())
