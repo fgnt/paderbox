2d3aba2f3a2605e64e7fa800ec18ec2275efcd8b
Jahn Heymann <heymann@nt.upb.de>
2016-01-12 09:53:42 +0100

    Use left and right context instead of window width

--- tests/utils_test/test_tbf_to_tbchw.py
+++ tests/utils_test/test_tbf_to_tbchw.py
@@ -8,6 +8,14 @@ class TestTBFtoTBCHW(unittest.TestCase):
     def setUp(self):
         self.data = np.random.uniform(0, 1, (30, 2, 5))
 
-    def test_shape(self):
-        x = tbf_to_tbchw(self.data, 3, 1)
-        self.assertEqual(x.shape, (30, 2, 1, 5, 3))
+    def test_shape_even_context(self):
+        x = tbf_to_tbchw(self.data, 3, 3, 1)
+        self.assertEqual(x.shape, (30, 2, 1, 5, 7))
+
+    def test_shape_left_context(self):
+        x = tbf_to_tbchw(self.data, 3, 0, 1)
+        self.assertEqual(x.shape, (30, 2, 1, 5, 4))
+
+    def test_shape_right_context(self):
+        x = tbf_to_tbchw(self.data, 0, 3, 1)
+        self.assertEqual(x.shape, (30, 2, 1, 5, 4))
