f1be4ec9cd503c991d81998aece4cf8ad6b0dd91
Jahn Heymann <heymann@nt.upb.de>
2016-01-25 07:19:22 +0100

    Make alignment optional so the fetcher can be used for decoding

--- tests/nn_tests/test_kaldi_data_fetcher.py
+++ tests/nn_tests/test_kaldi_data_fetcher.py
@@ -84,3 +84,62 @@ class TestKaldiDataFetcher(unittest.TestCase):
         self.assertEqual(data['x'].shape[0], data['ali'].shape[0])
         self.assertEqual(data['x'].ndim, 4)
         self.assertEqual(data['ali'].ndim, 2)
+
+    def test_utterance_mode_no_ali(self):
+        fetcher = KaldiDataFetcher('kaldi', SCP)
+        data = fetcher.get_data_for_indices((0,))
+        self.assertIn('x', data)
+        self.assertEqual(data['x'].shape[1], 13)
+
+    def test_utterance_mode_context_no_ali(self):
+        fetcher = KaldiDataFetcher('kaldi', SCP, left_context=2,
+                                   right_context=3)
+        data = fetcher.get_data_for_indices((0,))
+        self.assertIn('x', data)
+        self.assertEqual(data['x'].shape[1], 6*13)
+
+    def test_utterance_mode_sequential_no_ali(self):
+        fetcher = KaldiDataFetcher('kaldi', SCP,
+                                   sequence_features=True)
+        data = fetcher.get_data_for_indices((0,))
+        self.assertIn('x', data)
+        self.assertEqual(data['x'].ndim, 3)
+        self.assertEqual(data['x'].shape[2], 13)
+
+    def test_utterance_mode_sequential_context_no_ali(self):
+        fetcher = KaldiDataFetcher('kaldi', SCP,
+                                   sequence_features=True, left_context=2,
+                                   right_context=3)
+        data = fetcher.get_data_for_indices((0,))
+        self.assertIn('x', data)
+        self.assertEqual(data['x'].ndim, 3)
+        self.assertEqual(data['x'].shape[2], 6*13)
+
+    def test_frame_mode_no_ali(self):
+        fetcher = KaldiDataFetcher('kaldi', SCP, mode='frames')
+        data = fetcher.get_data_for_indices((0, 3, 10))
+        self.assertIn('x', data)
+        self.assertEqual(data['x'].shape[1], 13)
+        self.assertEqual(data['x'].shape[0], 3)
+        self.assertEqual(data['x'].ndim, 2)
+
+    def test_frame_mode_context_no_ali(self):
+        fetcher = KaldiDataFetcher('kaldi', SCP, mode='frames',
+                                   left_context=2, right_context=3)
+        data = fetcher.get_data_for_indices((0, 3, 10))
+        self.assertIn('x', data)
+        self.assertEqual(data['x'].shape[1], 6*13)
+        self.assertEqual(data['x'].shape[0], 3)
+        self.assertEqual(data['x'].ndim, 2)
+
+    def test_frame_mode_context_cnn_features_no_ali(self):
+        fetcher = KaldiDataFetcher('kaldi', SCP, mode='frames',
+                                   left_context=2, right_context=3,
+                                   cnn_features=True)
+        data = fetcher.get_data_for_indices((0, 3, 10))
+        self.assertIn('x', data)
+        self.assertEqual(data['x'].shape[1], 1)
+        self.assertEqual(data['x'].shape[2], 13)
+        self.assertEqual(data['x'].shape[3], 6)
+        self.assertEqual(data['x'].shape[0], 3)
+        self.assertEqual(data['x'].ndim, 4)
