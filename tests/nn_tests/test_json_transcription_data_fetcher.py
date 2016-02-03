9801c0275ed6877f1a701e56c478c5cec67424fa
Janek Ebbers <ebbers@nt.upb.de>
2016-02-03 16:55:46 +0100

    fixed test case

--- tests/nn_tests/test_json_transcription_data_fetcher.py
+++ tests/nn_tests/test_json_transcription_data_fetcher.py
@@ -2,19 +2,22 @@ import unittest
 import numpy
 from nt.nn.data_fetchers.json_transcription_data_fetcher \
     import JsonTranscriptionDataFetcher
+from nt.transcription_handling.transcription_handler import write_lexicon
 import json
 
 JSON_SRC = '/net/storage/2015/chime/chime_ref_data/data/json/chime.json'
+
 
 class JsonTranscriptionDataFetcherTest(unittest.TestCase):
 
     def setUp(self):
         with open(JSON_SRC) as fid:
             src = json.load(fid)
-        self.fetcher = JsonTranscriptionDataFetcher('chime',
-                                                    src,
-                            'train/A_database/flists/wav/channels_6/tr05_simu')
+        lexicon = write_lexicon(src)
+        self.fetcher = JsonTranscriptionDataFetcher(
+            'chime', src, 'train/A_database/flists/wav/channels_6/tr05_simu',
+            lexicon)
 
     def test_data_type(self):
-        data = self.fetcher.get_data_for_indices((0,))[0]
+        data = self.fetcher.get_data_for_indices((0,))['chime']
         self.assertEqual(data.dtype, numpy.int32)
