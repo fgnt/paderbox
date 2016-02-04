0782354570f74371b928d29f9f4ce97c09822f5d
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2016-02-04 18:46:14 +0100

    removed binary file (moved to /net/storage/python_unittest_data)

--- tests/speech_enhancement_test/generate_data_file.py
+++ tests/speech_enhancement_test/generate_data_file.py
@@ -1,37 +1,43 @@
 import numpy as np
-from nt.nn import DataProvider
-from nt.nn.data_fetchers import ChimeFeatureDataFetcher
 
-from nt.io.data_dir import chime as chime_dir
+from nt.io.data_dir import database_jsons as database_jsons_dir
+from nt.io.data_dir import testing as testing_dir
+from nt.nn import DataProvider
+from nt.nn.data_fetchers import JsonCallbackFetcher
+from nt.transform.module_stft import stft
+
 
 def main():
     common_setup = {
-        'json_src': chime_dir('data', 'json', 'chime.json'),
-        'flist': 'train/A_database/flists/wav/channels_6/tr05_simu',
-        'stft_shift': 256
+        'json_src': database_jsons_dir('chime.json'),
+        'flist': 'train/A_database/flists/wav/channels_6/tr05_simu'
     }
 
-    feature_channels = ['X/CH{}'.format(x) for x in range(1, 7)]
-    fetcher_X = ChimeFeatureDataFetcher('X', feature_channels=feature_channels, **common_setup)
-    feature_channels = ['N/CH{}'.format(x) for x in range(1, 7)]
-    fetcher_N = ChimeFeatureDataFetcher('N', feature_channels=feature_channels, **common_setup)
-    feature_channels = ['observed/CH{}'.format(x) for x in range(1, 7)]
-    fetcher_Y = ChimeFeatureDataFetcher('Y', feature_channels=feature_channels, **common_setup)
+    def callback_fcn(data_dict, **kwargs):
+        stft_opt = {"size": 1024, "shift": 256, "window_length": None}
+        return {key: stft(val, **stft_opt) for key, val in data_dict.items()}
+
+    fetcher = JsonCallbackFetcher('fetcher'
+                                  , feature_channels_glob=['N/CH*', 'X/CH*', 'observed/CH*']
+                                  , callback_fcn=callback_fcn, **common_setup)
+
     provider = DataProvider(
-        [fetcher_X, fetcher_N, fetcher_Y],
+        [fetcher],
         batch_size=1, max_queue_size=10, shuffle_data=False,
         sleep_time=1
     )
 
-    provider.__iter__()
-    ans = provider.__next__()
-    Y, X, N = ans['Y'], ans['X'], ans['N']
+    ans = provider.get_data_for_indices_tuple((1,))
+    Y, X, N = ans['observed'], ans['X'], ans['N']
+
     print('start')
-    np.savez('data', X=X, Y=Y, N=N)
-    with np.load('data.npz') as data:
+    file = testing_dir('speech_enhancement', 'data', 'beamformer')
+    np.savez(file, X=X, Y=Y, N=N)
+    with np.load(file + '.npz') as data:
         X = data['X']
     provider.shutdown()
     print('end')
+
 
 if __name__ == "__main__":
     main()
