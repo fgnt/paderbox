d5db39a07a443c58ba056b80f24b7a68a295b1ed
Lukas Drude <mail@lukas-drude.de>
2016-02-09 17:28:22 +0100

    PEP8

--- tests/speech_enhancement_test/test_noise.py
+++ tests/speech_enhancement_test/test_noise.py
@@ -1,12 +1,9 @@
 import unittest
 import numpy as np
 import nt.testing
-from scipy import signal
-
-import nt.testing as tc
-
 from nt.speech_enhancement.noise import set_snr
 from nt.speech_enhancement.noise import get_snr
+
 
 class TestNoiseMethods(unittest.TestCase):
     def test_set_and_get_single_source_snr(self):
@@ -16,7 +13,7 @@ class TestNoiseMethods(unittest.TestCase):
         N = np.random.normal(size=(T, F)) + 1j * np.random.normal(size=(T, F))
         X = 5*X
 
-        snr = 20.0 # dB
+        snr = 20.0  # dB
         X, N = set_snr(X, N, snr)
         calculated_snr = get_snr(X, N)
 
