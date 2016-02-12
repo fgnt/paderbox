00fd2d2c9461d52c74e1b03ab9b81720bccdb540
Janek Ebbers <ebbers@nt.upb.de>
2016-02-12 13:40:22 +0100

    use TemporaryDirectory as a context manager to clean up properly

--- tests/speech_recognition_tests/test_decoder.py
+++ tests/speech_recognition_tests/test_decoder.py
@@ -21,13 +21,6 @@ from nt.transcription_handling.transcription_handler import TranscriptionHandler
 
 
 class TestDecoder(unittest.TestCase):
-    def setUp(self):
-
-        self.tmpdir = tempfile.TemporaryDirectory()
-        print(self.tmpdir.name)
-
-    def tearDown(self):
-        self.tmpdir.cleanup()
 
     def write_net_out(self, trans_handler, label_seq, cost_along_path=None):
         trans_hat = np.zeros(
@@ -63,19 +56,8 @@ class TestDecoder(unittest.TestCase):
     # @unittest.skip("")
     def test_ground_truth(self):
 
-        working_dir = self.tmpdir.name
-
-        lm_path_uni = os.path.join(working_dir, 'tcb05cnp')
-        arpa.write_unigram(data_dir('speech_recognition', 'tcb05cnp'),
-                           lm_path_uni)
-
         lex = get_lexicon_from_arpa(data_dir('speech_recognition', 'tcb05cnp'))
         trans_handler = TranscriptionHandler(lex)
-
-        self.decoder = Decoder(trans_handler, working_dir,
-                               lm_file=lm_path_uni)
-
-        self.decoder.create_graphs()
 
         utt = "THIS SHOULD BE RECOGNIZED"
         utt_id = "TEST_UTT_1"
@@ -83,9 +65,19 @@ class TestDecoder(unittest.TestCase):
 
         trans_hat = self.write_net_out(trans_handler, label_seq, -1000)
 
-        self.decoder.create_lattices([trans_hat.num, ], [utt_id, ])
-        sym_decode, word_decode = \
-            self.decoder.decode(lm_scale=1, out_type='string')
+        with tempfile.TemporaryDirectory() as working_dir:
+
+            lm_path_uni = os.path.join(working_dir, 'tcb05cnp')
+            arpa.write_unigram(data_dir('speech_recognition', 'tcb05cnp'),
+                               lm_path_uni)
+
+            self.decoder = Decoder(trans_handler, working_dir,
+                                   lm_file=lm_path_uni)
+            self.decoder.create_graphs()
+            self.decoder.create_lattices([trans_hat.num, ], [utt_id, ])
+            sym_decode, word_decode = \
+                self.decoder.decode(lm_scale=1, out_type='string')
+
         print(sym_decode[utt_id])
         print(word_decode[utt_id])
         self.assertEqual(utt, word_decode[utt_id])
@@ -93,21 +85,10 @@ class TestDecoder(unittest.TestCase):
     # @unittest.skip("")
     def test_ground_truth_with_sil(self):
 
-        working_dir = self.tmpdir.name
-
-        lm_path_uni = os.path.join(working_dir, 'tcb05cnp')
-        arpa.write_unigram(data_dir('speech_recognition', 'tcb05cnp'),
-                           lm_path_uni)
-
         lex = get_lexicon_from_arpa(data_dir('speech_recognition', 'tcb05cnp'))
         trans_handler = TranscriptionHandler(lex)
         space = "<space>"
         trans_handler.label_handler.add_label(space)
-
-        self.decoder = Decoder(trans_handler, working_dir,
-                               lm_file=lm_path_uni, sil=space)
-
-        self.decoder.create_graphs()
 
         utt = "THIS SHOULD BE RECOGNIZED"
         utt_id = "TEST_UTT_1"
@@ -115,25 +96,27 @@ class TestDecoder(unittest.TestCase):
 
         trans_hat = self.write_net_out(trans_handler, label_seq, -1000)
 
-        self.decoder.create_lattices([trans_hat.num, ], [utt_id, ])
-        sym_decode, word_decode = \
-            self.decoder.decode(lm_scale=1, out_type='string')
+        with tempfile.TemporaryDirectory() as working_dir:
+            lm_path_uni = os.path.join(working_dir, 'tcb05cnp')
+            arpa.write_unigram(data_dir('speech_recognition', 'tcb05cnp'),
+                               lm_path_uni)
+            self.decoder = Decoder(trans_handler, working_dir,
+                                   lm_file=lm_path_uni, sil=space)
+            self.decoder.create_graphs()
+
+            self.decoder.create_lattices([trans_hat.num, ], [utt_id, ])
+            sym_decode, word_decode = \
+                self.decoder.decode(lm_scale=1, out_type='string')
+
         print(sym_decode[utt_id])
         print(word_decode[utt_id])
-
         self.assertEqual(utt, word_decode[utt_id])
 
     # @unittest.skip("")
     def test_only_lex(self):
 
-        working_dir = self.tmpdir.name
-
         lex = get_lexicon_from_arpa(data_dir('speech_recognition', 'tcb05cnp'))
         trans_handler = TranscriptionHandler(lex)
-
-        self.decoder = Decoder(trans_handler, working_dir)
-
-        self.decoder.create_graphs()
 
         utt = "THIS SHOULD BE RECOGNIZED"
         utt_id = "TEST_UTT_1"
@@ -141,9 +124,14 @@ class TestDecoder(unittest.TestCase):
 
         trans_hat = self.write_net_out(trans_handler, label_seq, -1000)
 
-        self.decoder.create_lattices([trans_hat.num, ], [utt_id, ])
-        sym_decode, word_decode = \
-            self.decoder.decode(lm_scale=1, out_type='string')
+        with tempfile.TemporaryDirectory() as working_dir:
+
+            self.decoder = Decoder(trans_handler, working_dir)
+            self.decoder.create_graphs()
+            self.decoder.create_lattices([trans_hat.num, ], [utt_id, ])
+            sym_decode, word_decode = \
+                self.decoder.decode(lm_scale=1, out_type='string')
+
         print(sym_decode[utt_id])
         print(word_decode[utt_id])
         self.assertEqual(utt, word_decode[utt_id])
@@ -152,10 +140,6 @@ class TestDecoder(unittest.TestCase):
     def test_compare_argmax_ctc(self):
 
         nn, trans_handler = self.load_model()
-
-        self.decoder = Decoder(trans_handler, self.tmpdir.name)
-        self.decoder.create_graphs()
-        self.decoder.decode_graph = self.decoder.ctc_map_fst
 
         json_path = database_jsons_dir('wsj.json')
         flist_test = 'test/flist/wave/official_si_dt_05'
@@ -177,52 +161,52 @@ class TestDecoder(unittest.TestCase):
         net_out_list = [net_out.num, ]
 
         utt_id = "TEST_UTT_1"
-        self.decoder.create_lattices(net_out_list, [utt_id, ])
-        sym_decode_int, word_decode_int = \
-            self.decoder.decode(lm_scale=1, out_type='ints')
 
-        word_decode = \
-            self.decoder.trans_handler.ints2labels(
-                word_decode_int["TEST_UTT_1"])
+        with tempfile.TemporaryDirectory() as working_dir:
+            self.decoder = Decoder(trans_handler, working_dir)
+            self.decoder.create_graphs()
+            self.decoder.decode_graph = self.decoder.ctc_map_fst
+            self.decoder.create_lattices(net_out_list, [utt_id, ])
+            sym_decode_int, word_decode_int = \
+                self.decoder.decode(lm_scale=1, out_type='ints')
+            word_decode = \
+                self.decoder.trans_handler.ints2labels(
+                    word_decode_int["TEST_UTT_1"])
 
         argmax_ctc = argmax_ctc_decode(net_out.num[:, 0, :],
                                        trans_handler.label_handler)
         print(word_decode)
         print(argmax_ctc)
-
         self.assertEqual(argmax_ctc, word_decode)
 
     # @unittest.skip("")
     def test_one_word_grammar(self):
 
-        working_dir = self.tmpdir.name
-
-        lex = get_lexicon_from_arpa(data_dir('speech_recognition', 'tcb05cnp'))
+        lex = get_lexicon_from_arpa(data_dir('speech_recognition',
+                                             'tcb05cnp'))
         trans_handler = TranscriptionHandler(lex)
 
         lm_file = data_dir('speech_recognition', "arpa_one_word")
-
-        self.decoder = Decoder(trans_handler, working_dir,
-                               lm_file=lm_file)
-        self.decoder.create_graphs()
 
         word = "TEST"
         utt_id = "TEST_UTT_1"
 
         trans_hat = self.write_net_out(trans_handler, word)
 
-        self.decoder.create_lattices([trans_hat.num, ], [utt_id, ])
-        sym_decode, word_decode = \
-            self.decoder.decode(lm_scale=1, out_type='string')
+        with tempfile.TemporaryDirectory() as working_dir:
+            self.decoder = Decoder(trans_handler, working_dir,
+                                   lm_file=lm_file)
+            self.decoder.create_graphs()
+            self.decoder.create_lattices([trans_hat.num, ], [utt_id, ])
+            sym_decode, word_decode = \
+                self.decoder.decode(lm_scale=1, out_type='string')
+
         print(sym_decode[utt_id])
         print(word_decode[utt_id])
-
         self.assertEqual(word, word_decode[utt_id])
 
     # @unittest.skip("")
     def test_lm_scale(self):
-
-        working_dir = self.tmpdir.name
 
         word1 = "ACOUSTIC"
         word2 = "LANGUAGE"
@@ -233,49 +217,49 @@ class TestDecoder(unittest.TestCase):
         lex = get_lexicon_from_arpa(lm_file)
         trans_handler = TranscriptionHandler(lex)
 
-        self.decoder = Decoder(trans_handler, working_dir,
-                               lm_file=lm_file)
-        self.decoder.create_graphs()
-
         trans_hat = self.write_net_out(trans_handler, word1, -1)
 
-        self.decoder.create_lattices([trans_hat.num, ], [utt_id, ])
+        with tempfile.TemporaryDirectory() as working_dir:
 
-        sym_decode, word_decode = \
-            self.decoder.decode(lm_scale=1.1, out_type='string')
-        print(sym_decode[utt_id])
-        print(word_decode[utt_id])
-        self.assertEqual(word2, word_decode[utt_id])
+            self.decoder = Decoder(trans_handler, working_dir,
+                                   lm_file=lm_file)
+            self.decoder.create_graphs()
+            self.decoder.create_lattices([trans_hat.num, ], [utt_id, ])
+            sym_decode_ac, word_decode_ac = \
+                self.decoder.decode(lm_scale=0.9, out_type='string')
+            sym_decode_lang, word_decode_lang = \
+                self.decoder.decode(lm_scale=1.1, out_type='string')
 
-        sym_decode, word_decode = \
-            self.decoder.decode(lm_scale=0.9, out_type='string')
-        print(sym_decode[utt_id])
-        print(word_decode[utt_id])
-        self.assertEqual(word1, word_decode[utt_id])
+        print(sym_decode_ac[utt_id])
+        print(word_decode_ac[utt_id])
+        self.assertEqual(word1, word_decode_ac[utt_id])
+
+        print(sym_decode_lang[utt_id])
+        print(word_decode_lang[utt_id])
+        self.assertEqual(word2, word_decode_lang[utt_id])
 
     # @unittest.skip("")
     def test_trigram_grammar(self):
 
-        working_dir = self.tmpdir.name
-
-        lex = get_lexicon_from_arpa(data_dir('speech_recognition', 'tcb05cnp'))
+        lex = get_lexicon_from_arpa(data_dir('speech_recognition',
+                                             'tcb05cnp'))
         trans_handler = TranscriptionHandler(lex)
-
-        lm_file = data_dir('speech_recognition', "arpa_three_words_tri")
-
-        self.decoder = Decoder(trans_handler, working_dir,
-                               lm_file=lm_file)
-
-        self.decoder.create_graphs()
 
         utt_id = "TEST_UTT_1"
         utt = "SHE SEES"
         symbol_seq = "SHE___SE_ES"
         trans_hat = self.write_net_out(trans_handler, symbol_seq, -10)
+        lm_file = data_dir('speech_recognition', "arpa_three_words_tri")
 
-        self.decoder.create_lattices([trans_hat.num, ], [utt_id, ])
-        sym_decode, word_decode = \
-            self.decoder.decode(lm_scale=1, out_type='string')
+        with tempfile.TemporaryDirectory() as working_dir:
+
+            self.decoder = Decoder(trans_handler, working_dir,
+                                   lm_file=lm_file)
+            self.decoder.create_graphs()
+            self.decoder.create_lattices([trans_hat.num, ], [utt_id, ])
+            sym_decode, word_decode = \
+                self.decoder.decode(lm_scale=1, out_type='string')
+
         print(sym_decode[utt_id])
         print(word_decode[utt_id])
         self.assertEqual(utt, word_decode[utt_id])
@@ -283,44 +267,48 @@ class TestDecoder(unittest.TestCase):
     # @unittest.skip("")
     def test_oov(self):
 
-        working_dir = self.tmpdir.name
+        with tempfile.TemporaryDirectory() as working_dir:
 
-        word1 = "WORKS"
-        word2 = "KORKS"
+            word1 = "WORKS"
+            word2 = "KORKS"
 
-        lex_file = os.path.join(working_dir, 'lexicon.txt')
-        with open(lex_file, 'w') as fid:
-            fid.write(word1)
-            for letter in word1:
-                fid.write(" " + letter)
+            lex_file = os.path.join(working_dir, 'lexicon.txt')
+            with open(lex_file, 'w') as fid:
+                fid.write(word1)
+                for letter in word1:
+                    fid.write(" " + letter)
 
-        lex = get_lexicon_from_txt(lex_file)
-        trans_handler = TranscriptionHandler(lex)
+            lex = get_lexicon_from_txt(lex_file)
+            trans_handler = TranscriptionHandler(lex)
 
-        lm_path_uni = os.path.join(working_dir, 'tcb05cnp')
-        arpa.write_unigram(data_dir('speech_recognition', 'tcb05cnp'),
-                           lm_path_uni)
+            lm_path_uni = os.path.join(working_dir, 'tcb05cnp')
+            arpa.write_unigram(data_dir('speech_recognition', 'tcb05cnp'),
+                               lm_path_uni)
 
-        self.decoder = Decoder(trans_handler, working_dir,
-                               lm_file=lm_path_uni)
-        self.decoder.create_graphs()
+            self.decoder = Decoder(trans_handler, working_dir,
+                                   lm_file=lm_path_uni)
+            self.decoder.create_graphs()
 
-        utt_id = "TEST_UTT_1"
-        utt_length = len(word1)
+            utt_id = "TEST_UTT_1"
+            utt_length = len(word1)
 
-        trans_hat = np.zeros((utt_length, 1, len(trans_handler.label_handler)))
-        for idx in range(len(word1)):
-            sym1 = word1[idx]
-            sym2 = word2[idx]
-            trans_hat[idx, 0, trans_handler.label_handler.label_to_int[sym1]]\
-                += 5
-            trans_hat[idx, 0, trans_handler.label_handler.label_to_int[sym2]]\
-                += 10
-        trans_hat = Variable(trans_hat)
+            trans_hat = np.zeros((utt_length, 1,
+                                  len(trans_handler.label_handler)))
+            for idx in range(len(word1)):
+                sym1 = word1[idx]
+                sym2 = word2[idx]
+                trans_hat[idx, 0,
+                          trans_handler.label_handler.label_to_int[sym1]]\
+                    += 5
+                trans_hat[idx, 0,
+                          trans_handler.label_handler.label_to_int[sym2]]\
+                    += 10
+            trans_hat = Variable(trans_hat)
 
-        self.decoder.create_lattices([trans_hat.num, ], [utt_id, ])
-        sym_decode, word_decode = \
-            self.decoder.decode(lm_scale=1, out_type='string')
+            self.decoder.create_lattices([trans_hat.num, ], [utt_id, ])
+            sym_decode, word_decode = \
+                self.decoder.decode(lm_scale=1, out_type='string')
+
         print(sym_decode[utt_id])
         print(word_decode[utt_id])
 
