5b5f130f06d46dcd654bdc1b8be6791fb47d7503
Prerna Arora <arora@nt.upb.de>
2016-03-01 14:13:35 +0100

    Added median filter

--- nt/transform/module_filter.py
+++ nt/transform/module_filter.py
@@ -1,7 +1,7 @@
 """
 Provides general filters, for example preemphasis filter.
 """
-from scipy.signal import lfilter
+from scipy.signal import lfilter, medfilt
 
 
 def preemphasis(time_signal, p=0.95):
@@ -23,3 +23,13 @@ def offset_compensation(time_signal):
     Offset compensation filter.
     """
     return lfilter([1., -1], [1., -0.999], time_signal)
+
+
+def median(input, window_size=3):
+    """
+    Median Filter
+    :param input:array of values to be filtered
+    :param window_size: kernel size for the filter
+    :return: filtered output array of same length as input
+    """
+    return medfilt(input, window_size)
