3b558c2e2e1530b330552ca454fda62a0496123a
Oliver Walter <walter@nt.uni-paderborn.de>
2016-03-03 11:21:01 +0100

    Added linear lexicon

--- tests/speech_recognition_tests/test_lexicon.py
+++ tests/speech_recognition_tests/test_lexicon.py
@@ -2,36 +2,56 @@ __author__ = 'walter'
 import unittest
 import tempfile
 from nt.speech_recognition.lexicon import Trie
+from nt.speech_recognition.lexicon import Linear
 from nt.transcription_handling.transcription_handler import TranscriptionHandler
 from nt.speech_recognition import fst
 
 class TestLexicon(unittest.TestCase):
 
-    def test_write_fst(self):
-        words = ['Mars', 'man', 'Martian', 'Marsman']
-        lexicon = {word: list(word) for word in words}
+    def setUp(self):
+#        self.words = ['Mars', 'man', 'Martian', 'Marsman']
+        self.words = ['AA', 'AB', 'AB']
+        self.lexicon = {word: list(word) for word in self.words}
 
-        special_symbols = {'blank': '<blank>', 'eps': '<eps>', 'phi': '<phi>', 'sow': '<sow>',
-                           'eow': '</eow>', 'sos': '<sos>', 'eos': '</eos>'}
-        transcription_handler = TranscriptionHandler(lexicon, **special_symbols)
+        self.special_symbols = {'blank': '<blank>', 'eps': '<eps>', 'phi': '<phi>', 'sow': '<sow>',
+                                'eow': '</eow>', 'sos': '<sos>', 'eos': '</eos>'}
+        self.transcription_handler = TranscriptionHandler(self.lexicon, **self.special_symbols)
 
-        word_offset = len(transcription_handler.label_handler)
-        int_lexicon = {transcription_handler.words2ints(word)[0] + word_offset:
-                           transcription_handler.labels2ints(labels) for word, labels in lexicon.items()}
-        int_eps = transcription_handler.labels2ints(special_symbols['eps'])[0]
-        int_eos = transcription_handler.labels2ints(special_symbols['eow'])[0]
-        int_label = [transcription_handler.labels2ints(label)[0] for label in transcription_handler.label_handler.labels]
+        self.word_offset = len(self.transcription_handler.label_handler)
+        self.int_lexicon = {self.transcription_handler.words2ints(word)[0] + self.word_offset:
+                            self.transcription_handler.labels2ints(labels) for word, labels in self.lexicon.items()}
+        self.int_eps = self.transcription_handler.labels2ints(self.special_symbols['eps'])[0]
+        self.int_eos = self.transcription_handler.labels2ints(self.special_symbols['eow'])[0]
+        self.int_label = [self.transcription_handler.labels2ints(label)[0]
+                          for label in self.transcription_handler.label_handler.labels]
 
-        word_trie = Trie(int_eps, int_eos)
-        for word in int_lexicon.items():
+
+    def test_trie_write_fst(self):
+        word_trie = Trie(self.int_eps, self.int_eos)
+        for word in self.int_lexicon.items():
             word_trie.add_word(word)
 
-        word_trie.build_character_model(int_label)
+        word_trie.build_character_model(self.int_label)
 
         with tempfile.TemporaryDirectory() as directory:
             fst_filename = directory + '/L.fst'
             sym_filename = fst_filename + '.syms'
             pdf_filename = fst_filename + '.pdf'
             word_trie.write_fst(fst_filename)
-            transcription_handler.write_table(sym_filename)
+            self.transcription_handler.write_table(sym_filename)
+            fst.draw(sym_filename, sym_filename, fst_filename, pdf_filename)
+
+
+    def test_linear_add_words(self):
+        word_linear = Linear(self.int_eps, self.int_eos)
+        word_linear.add_words(self.int_lexicon)
+
+        word_linear.build_language_model(self.int_label)
+
+        with tempfile.TemporaryDirectory() as directory:
+            fst_filename = directory + '/L.fst'
+            sym_filename = fst_filename + '.syms'
+            pdf_filename = fst_filename + '.pdf'
+            word_linear.write_fst(fst_filename)
+            self.transcription_handler.write_table(sym_filename)
             fst.draw(sym_filename, sym_filename, fst_filename, pdf_filename)
