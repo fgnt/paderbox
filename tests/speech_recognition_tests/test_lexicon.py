cd77b32c28c802ed0173ad55a47dc2b8b12767c8
Oliver Walter <walter@nt.uni-paderborn.de>
2016-03-07 20:38:14 +0100

    remove trie lexicon since all the functionality is accesable by setting correct parameters in linear lexicon

--- tests/speech_recognition_tests/test_lexicon.py
+++ tests/speech_recognition_tests/test_lexicon.py
@@ -1,7 +1,6 @@
 __author__ = 'walter'
 import unittest
 import tempfile
-from nt.speech_recognition.lexicon import Trie
 from nt.speech_recognition.lexicon import Linear
 from nt.transcription_handling.transcription_handler import TranscriptionHandler
 from nt.speech_recognition import fst
@@ -26,16 +25,17 @@ class TestLexicon(unittest.TestCase):
         self.int_eps = self.transcription_handler.labels2ints(self.special_symbols['eps'])[0]
         self.int_eow = self.transcription_handler.labels2ints(self.special_symbols['eow'])[0]
         self.int_phi = self.transcription_handler.labels2ints(self.special_symbols['phi'])[0]
+        self.int_sow = self.transcription_handler.labels2ints(self.special_symbols['sow'])[0]
         self.int_eos_label = self.transcription_handler.labels2ints(self.special_symbols['eos'])[0]
         self.int_label = [self.transcription_handler.labels2ints(label)[0]
                           for label in self.transcription_handler.label_handler.labels]
 
-    def _test_write_fst(self, class_type):
+    def _test_write_fst(self, class_type, add_word_mode, build_character_model_mode):
         lexicon = class_type(self.int_eps, self.int_eow)
         for word in self.int_lexicon.items():
-            lexicon.add_word(word)
+            lexicon.add_word(word, add_word_mode)
 
-        lexicon.build_character_model(self.int_label)
+        lexicon.build_character_model(self.int_label, build_character_model_mode, self.int_sow)
         lexicon.add_eos(self.int_eos_label, self.int_eos_word)
         lexicon.add_self_loops(self.int_phi)
 
@@ -47,8 +47,26 @@ class TestLexicon(unittest.TestCase):
             self.transcription_handler.write_table(sym_filename)
             fst.draw(sym_filename, sym_filename, fst_filename, pdf_filename)
 
-    def test_trie_write_fst(self):
-        self._test_write_fst(Trie)
+    def test_linear_linear_trie_write_fst(self):
+        self._test_write_fst(Linear, 'linear', 'trie')
 
-    def test_linear_write_fst(self):
-        self._test_write_fst(Linear)
+    def test_linear_linear_flat_write_fst(self):
+        self._test_write_fst(Linear, 'linear', 'flat')
+
+    def test_linear_linear_copy_write_fst(self):
+        self._test_write_fst(Linear, 'linear', 'copy')
+
+    def test_linear_linear_linear_write_fst(self):
+        self._test_write_fst(Linear, 'linear', 'linear')
+
+    def test_linear_trie_trie_write_fst(self):
+        self._test_write_fst(Linear, 'trie', 'trie')
+
+    def test_linear_trie_flat_write_fst(self):
+        self._test_write_fst(Linear, 'trie', 'flat')
+
+    def test_linear_trie_copy_write_fst(self):
+        self._test_write_fst(Linear, 'trie', 'copy')
+
+    def test_linear_trie_linear_write_fst(self):
+        self._test_write_fst(Linear, 'trie', 'linear')
