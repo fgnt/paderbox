a22ef511765a958746f38f80ac8bee20557b2dc7
Oliver Walter <walter@nt.uni-paderborn.de>
2016-03-30 13:59:23 +0200

    - reformating to a maximum width of 80 characters

--- tests/speech_recognition_tests/test_lexicon.py
+++ tests/speech_recognition_tests/test_lexicon.py
@@ -11,32 +11,47 @@ class TestLexicon(unittest.TestCase):
 #        self.words = ['Mars', 'man', 'Martian', 'Marsman']
         self.words = ['AA', 'AB']
         self.lexicon = {word: list(word) for word in self.words}
-        self.special_symbols = {'blank': None, 'eps': '<eps>', 'phi': '<phi>', 'sow': '<sow>',
-                                'eow': '</eow>', 'eos': '</eos>', 'eoc': '</eoc>'}
+        self.special_symbols = {'blank': None, 'eps': '<eps>', 'phi': '<phi>',
+                                'sow': '<sow>', 'eow': '</eow>',
+                                'eos': '</eos>', 'eoc': '</eoc>'}
 
-        self.transcription_handler = TranscriptionHandler(self.lexicon, **self.special_symbols)
-        self.transcription_handler.add_word(self.special_symbols['eos'], [self.special_symbols['eos']])
+        self.transcription_handler = TranscriptionHandler(
+            self.lexicon, **self.special_symbols)
+        self.transcription_handler.add_word(self.special_symbols['eos'],
+                                            [self.special_symbols['eos']])
 
         self.word_offset = len(self.transcription_handler.label_handler)
-        self.int_lexicon = {self.transcription_handler.words2ints(word)[0] + self.word_offset:
-                            self.transcription_handler.labels2ints(labels) for word, labels in self.lexicon.items()}
-        self.int_eos_word = self.transcription_handler.words2ints(self.special_symbols['eos'])[0] + self.word_offset
+        self.int_lexicon = {self.transcription_handler.words2ints(word)[0] +
+                            self.word_offset:
+                            self.transcription_handler.labels2ints(labels)
+                            for word, labels in self.lexicon.items()}
+        self.int_eos_word = self.transcription_handler.words2ints(
+            self.special_symbols['eos'])[0] + self.word_offset
 
-        self.int_eps = self.transcription_handler.labels2ints(self.special_symbols['eps'])[0]
-        self.int_eow = self.transcription_handler.labels2ints(self.special_symbols['eow'])[0]
-        self.int_phi = self.transcription_handler.labels2ints(self.special_symbols['phi'])[0]
-        self.int_sow = self.transcription_handler.labels2ints(self.special_symbols['sow'])[0]
-        self.int_eoc = self.transcription_handler.labels2ints(self.special_symbols['eoc'])[0]
-        self.int_eos_label = self.transcription_handler.labels2ints(self.special_symbols['eos'])[0]
+        self.int_eps = self.transcription_handler.labels2ints(
+            self.special_symbols['eps'])[0]
+        self.int_eow = self.transcription_handler.labels2ints(
+            self.special_symbols['eow'])[0]
+        self.int_phi = self.transcription_handler.labels2ints(
+            self.special_symbols['phi'])[0]
+        self.int_sow = self.transcription_handler.labels2ints(
+            self.special_symbols['sow'])[0]
+        self.int_eoc = self.transcription_handler.labels2ints(
+            self.special_symbols['eoc'])[0]
+        self.int_eos_label = self.transcription_handler.labels2ints(
+            self.special_symbols['eos'])[0]
         self.int_label = [self.transcription_handler.labels2ints(label)[0]
-                          for label in self.transcription_handler.label_handler.labels]
+                          for label in
+                          self.transcription_handler.label_handler.labels]
 
-    def _test_write_fst(self, class_type, add_word_mode, build_character_model_mode, sow=None, eoc=None):
+    def _test_write_fst(self, class_type, add_word_mode,
+                        build_character_model_mode, sow=None, eoc=None):
         lexicon = class_type(self.int_eps, self.int_eow, eoc)
         for word in self.int_lexicon.items():
             lexicon.add_word(word, add_word_mode)
 
-        lexicon.build_character_model(self.int_label, build_character_model_mode, sow)
+        lexicon.build_character_model(self.int_label,
+                                      build_character_model_mode, sow)
         lexicon.add_eos(self.int_eos_label, self.int_eos_word)
         lexicon.add_self_loops(self.int_phi)
 
