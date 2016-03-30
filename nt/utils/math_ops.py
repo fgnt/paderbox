3ce09057ef052681c99e84cece469898cc0ef4a0
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2016-03-30 18:10:25 +0200

    add cart2sph and sph2cart

--- nt/utils/math_ops.py
+++ nt/utils/math_ops.py
@@ -118,3 +118,19 @@ def _calculate_block_boundaries(T, block_size, first_block_size):
         block_start = block_end
         block_end = np.minimum(block_start + block_size, T)
     return block_boundaries
+
+
+# http://stackoverflow.com/questions/30084174/efficient-matlab-cart2sph-and-sph2cart-functions-in-python
+def cart2sph(x, y, z):
+    azimuth = np.arctan2(y, x)
+    elevation = np.arctan2(z, np.sqrt(x**2 + y**2))
+    r = np.sqrt(x**2 + y**2 + z**2)
+    return azimuth, elevation, r
+
+
+# http://stackoverflow.com/questions/30084174/efficient-matlab-cart2sph-and-sph2cart-functions-in-python
+def sph2cart(azimuth, elevation, r):
+    x = r * np.cos(elevation) * np.cos(azimuth)
+    y = r * np.cos(elevation) * np.sin(azimuth)
+    z = r * np.sin(elevation)
+    return x, y, z
