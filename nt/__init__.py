f99b69502e4be72ef74ca5f33495610bdc36c5a1
Janek Ebbers <ebbers@nt.upb.de>
2016-04-07 08:46:34 +0200

    make sure to use threads

--- nt/__init__.py
+++ nt/__init__.py
@@ -1,2 +1,5 @@
 # print('Hey there. A documentation is available here:\n'
 #       'http://ntjenkins.upb.de/view/PythonToolbox/job/python_toolbox/Documentation/')
+import mkl
+
+mkl.set_num_threads(mkl.get_max_threads())
