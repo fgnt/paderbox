36384cef14bb52eade00008e75b5cd16d87e47f3
Thomas Glarner <tglarner@mail.upb.de>
2016-04-12 18:22:39 +0200

    Make mkl import optional. This may prevent omp multithreading. 	But at least other linear algebra packages still work.

--- nt/__init__.py
+++ nt/__init__.py
@@ -1,5 +1,10 @@
 # print('Hey there. A documentation is available here:\n'
 #       'http://ntjenkins.upb.de/view/PythonToolbox/job/python_toolbox/Documentation/')
-import mkl
 
-mkl.set_num_threads(mkl.get_max_threads())
+# if mkl is available, set maximum number of threads
+try:
+    import mkl
+    mkl.set_num_threads(mkl.get_max_threads())
+except ImportError:
+    pass
+
