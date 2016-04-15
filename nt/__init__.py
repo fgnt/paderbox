8a38aeb91afd2e2c687e29d0f21f642cefbe5d9c
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2016-04-15 09:44:38 +0200

    Bugfix: suppress ImportError if mkl is not installed

--- nt/__init__.py
+++ nt/__init__.py
@@ -1,10 +1,8 @@
 # print('Hey there. A documentation is available here:\n'
 #       'http://ntjenkins.upb.de/view/PythonToolbox/job/python_toolbox/Documentation/')
+from contextlib import suppress
 
 # if mkl is available, set maximum number of threads
-try:
+with suppress(ImportError):
     import mkl
     mkl.set_num_threads(mkl.get_max_threads())
-except ImportError:
-    pass
-
