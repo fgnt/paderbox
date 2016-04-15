dc447a5d897f52d656c29f0c27e98c55f110003e
Prerna Arora <arora@nt.upb.de>
2016-04-15 10:51:32 +0200

    Removed parts with dependencies on 'with context' parameter.

--- tests/nn_tests/test_array_data_fetcher.py
+++ tests/nn_tests/test_array_data_fetcher.py
@@ -15,24 +15,12 @@ class ArrayDataFetcherTest(unittest.TestCase):
                                batch_size=2,
                                max_queue_size=5,
                                shuffle_data=False)
-        ## With Context
-        self.data_3 = numpy.repeat(numpy.arange(5)[None, :], 10, axis=0)
-        self.data_4 = numpy.repeat(numpy.arange(1, 0, -1)[None, :], 10, axis=0)
-        self.bins = numpy.array([0, self.data_3.shape[0]])
-        self.fetcher_3 = ArrayDataFetcher('Xc', self.data_3, self.bins, with_context=True)
-        self.fetcher_4 = ArrayDataFetcher('Yc', self.data_4)
-        self.dp_2 = DataProvider((self.fetcher_3, self.fetcher_4),
-                                 batch_size=2,
-                                 max_queue_size=5,
-                                 shuffle_data=False)
-
 
     def test_setup(self):
         self.assertEqual(len(self.dp), 5)
-        self.assertEqual(len(self.dp_2), 5)
 
     def test_iteration(self):
-        for idx, batch_data in enumerate(self.dp.iterate()):
+        for idx, batch_data in enumerate(self.dp.iterate(fork_fetchers=False)):
             self.assertTrue('X' in batch_data)
             self.assertTrue('Y' in batch_data)
             numpy.testing.assert_equal(batch_data['X'],
@@ -53,13 +41,3 @@ class ArrayDataFetcherTest(unittest.TestCase):
     def test_get_shape(self):
         s = self.fetcher_1.get_data_shape()
         self.assertEqual(s, {'X': (1, 5)})
-
-    def test_data_withContext(self):
-        batch = self.dp_2.test_run()
-        for data in batch.values():
-            if isinstance(data, numpy.ndarray):
-                self.assertTrue(data.flags.c_contiguous)
-
-    def test_get_shape_withContext(self):
-        s = self.fetcher_3.get_data_shape()
-        self.assertEqual(s, {'Xc': (1, 55)})
