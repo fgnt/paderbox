9e91a419952fcd1ad09eba17c69ee506937bc2e8
Oliver Walter <walter@nt.uni-paderborn.de>
2016-05-06 13:29:52 +0200

    extend dtw to make border condition configurable (this can be used for insertions/deletions with levenstein distance or to allow any starting point) add parameter for additional transformation function from distance matrix to cost matrix - usefull if we want to work with distances and e.g. maximize the distence (minimize negative distance + bias) add optional penalities for diagonal, vertical and horizontal steps add optional scaling of weights when taking different steps (usefull to imitate behaviour of levensthein distance where you only add on a diagonal step)

--- nt/utils/dtw.py
+++ nt/utils/dtw.py
@@ -1,19 +1,30 @@
-from numpy import array, zeros, argmin, inf
+from numpy import array, zeros, argmin, inf, arange
 
-def dtw(x, y, dist):
+def dtw(x, y, dist, dist_to_cost=None, border=(inf, inf), penalty=(0, 0, 0),
+        weight=(1, 1, 1)):
     """
     Computes Dynamic Time Warping (DTW) of two sequences.
     :param x: N1*M array or N1 element list
     :param y: N2*M array or N2 element list
-    :param dist: distance function to calculate distance
-    :return: minimum distance, cost matrix, accumulated cost matrix, wrap path
+    :param dist: distance function to calculate between elements of x and y
+    :param dist_to_cost: transformation from distance to cost matrix.
+                         needs to be an in place operation
+    :param border: cost to add for steps along the border: (repeat x, repeat y)
+    :param penalty: penalty for steps: (repeat x, match, repeat y)
+    :param cost_scaling: scaling factor for costs when taking step:
+                         (repeat x, match, repeat y)
+                        'for usual dtw set (0, 0, 0)
+                        'for levensthein': add distances only at diagonal
+                                        step. always add penalty
+                                        --> set (0, 1, 0)
+    :return: minimum cost, distance matrix, accumulated cost matrix, wrap path
     """
 
-    # init
+    # init (boundary condition: start at (0,0) --> set boundaries to border)
     r, c = len(x), len(y)
     D0 = zeros((r + 1, c + 1))
-    D0[0, 1:] = inf
-    D0[1:, 0] = inf
+    D0[0, 1:] = arange(1, c + 1) * border[0]
+    D0[1:, 0] = arange(1, r + 1) * border[1]
 
     # calculate distance matrix
     D1 = D0[1:, 1:] # operate on view for easy indexing
@@ -21,39 +32,76 @@ def dtw(x, y, dist):
         for j in range(c):
             D1[i, j] = dist(x[i], y[j])
 
+    # copy for output, since we are going to overwrite D0 and D1
+    C = D1.copy()
+
+    # normalize distance matrix
+    if dist_to_cost is not None:
+        dist_to_cost(D1)
+
     # calculate accumulative distance matrix
-    C = D1.copy() # copy for output, since we are going to overwrite D and D1
+    # note: we again operate on view
+    # --> use D0 instead of i and/or j minus 1
     for i in range(r):
         for j in range(c):
-            D1[i, j] += min(D0[i, j], D0[i, j+1], D0[i+1, j])
+            D1[i, j] += min(weight[1] * D0[i, j] + penalty[1],
+                            weight[2] * D0[i, j+1] + penalty[2],
+                            weight[0] * D0[i+1, j] + penalty[0])
 
     # traceback
-    if len(x)==1:
-        path = zeros(len(y)), range(len(y))
-    elif len(y) == 1:
-        path = range(len(x)), zeros(len(x))
-    else:
-        path = _traceback(D0)
+    path = _traceback(D0, penalty)
 
-    return D1[-1, -1] / sum(D1.shape), C, D1, path
+    return D1[-1, -1], C, D1, path
 
-def _traceback(D):
+def _traceback(D, penalty=(0, 0, 0)):
     """
-    compute traceback through distance matrix
-    :param D: distance matrix
+    compute traceback through distance matrix starting from the end of both
+    sequences (not partial matching)
+    :param D: distance matrix (including boundary conditions)
+    :param penalty: penalty for steps: (repeat x, match, repeat y)
     :return: path pairs
     """
+
+    # starting index i, j = len(x) - 1, len(y) - 1
+    # (calculated on distance matrix with boundary condition, therefore -2)
+    # this also allows for easy checking for (i,j), (i,j+1), (i+1,j) in D
+    # is equivalent to checking for (i-1,j-1), (i-1,j), (i,j-1) in D1
     i, j = array(D.shape) - 2
     p, q = [i], [j]
     while ((i > 0) or (j > 0)):
-        tb = argmin((D[i, j], D[i, j+1], D[i+1, j]))
-        if (tb == 0):
+        tb = argmin((D[i, j] + penalty[1],
+                     D[i, j+1] + penalty[2],
+                     D[i+1, j] + penalty[0]))
+        if tb == 0 and i > 0 and j > 0:
             i -= 1
             j -= 1
-        elif (tb == 1):
+        elif tb == 1 and i > 0:
             i -= 1
-        else: # (tb == 2):
+        elif tb == 2 and j > 0: # (tb == 2):
             j -= 1
+        else:
+            break
+
+        # insert index in front of list, since we are going backwards
+        p.insert(0, i)
+        q.insert(0, j)
+
+    # consume remaining indices in input sequences, if boundary was crossed
+    # (insertions/deletions at befinning)
+    while (i >= -1 and j > 0) or (i > 0 and j >= -1):
+        if tb == 0:
+            i -= 1
+            j -= 1
+            if j == -1:
+                tb = 1
+            else:
+                tb = 2
+        elif tb == 1:
+            i -= 1
+        else:
+            j -= 1
+
+        # insert index in front of list, since we are going backwards
         p.insert(0, i)
         q.insert(0, j)
 
