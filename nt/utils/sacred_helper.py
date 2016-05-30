18eac28c609c4e9e76d0182cc4ffeca5f4423a57
Jahn Heymann <heymann@nt.upb.de>
2016-05-30 11:05:44 +0200

    Allow to specify database and prefix

--- nt/utils/sacred_helper.py
+++ nt/utils/sacred_helper.py
@@ -7,7 +7,7 @@ def get_sacred_uri_from_file(secret_file=None):
     """
 
     Store a mongodb uri in a file. I recommend `~/.sacred`. The file should
-    contain i.e. `mongodb://user:password@131.234.222.24:10135/sacred`.
+    contain i.e. `mongodb://user:password@131.234.222.24:10135`.
 
     Args:
         secret_file: Optional path to your sacred secret.
@@ -22,9 +22,12 @@ def get_sacred_uri_from_file(secret_file=None):
     return uri
 
 
-def get_config_from_id(_id, secret_file=None):
+def get_config_from_id(_id, database='sacred', prefix='default',
+                       secret_file=None):
     uri = get_sacred_uri_from_file(secret_file)
+    uri += '/' if uri[-1] != '/' else ''
+    uri += database
     client = MongoClient(uri)
-    runs = client.sacred.default.runs
+    runs = client[database][prefix].runs
     experiment = runs.find_one({'_id': ObjectId(_id)})
     return experiment['config']
