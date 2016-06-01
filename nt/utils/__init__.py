c99ddc190ad036f654c0f3a2844082dacbdfb58b
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2016-06-01 20:04:14 +0200

    remove one star import

--- nt/utils/__init__.py
+++ nt/utils/__init__.py
@@ -1,9 +1,7 @@
 from nt.utils.file_handling import *
-from nt.utils.timer import *
+from nt.utils.timer import Timer, timeStamped
 from nt.utils.container import Container
 from nt.utils.json_utils import *
 
 mkdir_p = mkdir_p
-Timer = Timer
-timeStamped = timeStamped
 Container = Container
