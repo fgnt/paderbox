56737917d37aa2b4f0e2ce52cc080ef171bd424c
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2016-06-20 10:42:49 +0200

    matlab: Changed to student licence

--- nt/utils/matlab.py
+++ nt/utils/matlab.py
@@ -29,8 +29,9 @@ class Mlab():
         if 'nt' in hostname:
             mlab_process = Matlab(
                 'nice -n 3 ' +
-                '/net/ssd/software/MATLAB/R2015a/bin/matlab ' +
-                '-nodisplay -nosplash'
+                '/net/ssd/software/MATLAB/R2015a/bin/matlab' +
+                ' -c /opt/MATLAB/R2014a_studis/licenses/network.lic'
+                ' -nodisplay -nosplash'
             )
         else:
             mlab_process = Matlab('nice -n 3 matlab -nodisplay -nosplash')
