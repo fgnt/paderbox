1c7b73720d9fbb97e081d573f0fc45f585022d31
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2016-07-08 10:42:33 +0200

    update utils.__init__

--- nt/utils/__init__.py
+++ nt/utils/__init__.py
@@ -1,7 +1,9 @@
+from .deprecated import deprecated
 from nt.utils.file_handling import *
 from nt.utils.timer import Timer, timeStamped
-from nt.utils.container import Container
+from nt.utils.container import Container, AttrDict
 from nt.utils.json_utils import *
+
 
 mkdir_p = mkdir_p
 Container = Container
