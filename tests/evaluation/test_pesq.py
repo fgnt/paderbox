52c1d7a58694cd211eb0ae8e079498ac1408fb3a
Lukas Drude <mail@lukas-drude.de>
2016-08-15 20:12:20 +0200

    Changed input and output to PESQ. This is a braking change. Possible input is now: list, ndarray or path string. Output ONLY contains MOS-LQO prediction score.

--- tests/evaluation/test_pesq.py
+++ tests/evaluation/test_pesq.py
@@ -1,5 +1,5 @@
 import unittest
-from nt.evaluation.pesq import pesq, threaded_pesq
+from nt.evaluation.pesq import pesq
 import numpy
 import numpy.testing as nptest
 from nt.io.audioread import audioread
@@ -7,29 +7,84 @@ import os
 import inspect
 
 
-class TestPESQ(unittest.TestCase):
-
+class TestProposedPESQ(unittest.TestCase):
+    """
+    This test case was written before the code was adapted.
+    This is, why it fails.
+    """
     def setUp(self):
         current_dir = os.path.dirname(
             os.path.abspath(inspect.getfile(inspect.currentframe())))
 
-        self.ref = os.path.join(current_dir, 'data/speech.wav')
-        self.deg = os.path.join(current_dir, 'data/speech_bab_0dB.wav')
-        self.refer = audioread(self.ref)
-        self.rate = 16000
+        self.ref_path = os.path.join(current_dir, 'data/speech.wav')
+        self.deg_path = os.path.join(current_dir, 'data/speech_bab_0dB.wav')
 
-    def test_wb_scores(self):
-        scores = pesq(self.ref, self.deg, 'wb', self.rate)
-        nptest.assert_equal(scores, numpy.asarray([[0, 1.083]]))
+        self.ref_array = audioread(self.ref_path)
+        self.deg_array = audioread(self.deg_path)
 
-    def test_nb_scores(self):
-        scores = pesq(self.ref, self.deg, 'nb', self.rate)
-        nptest.assert_equal(scores, numpy.asarray([[1.969, 1.607]]))
+    def test_wb_scores_with_lists_of_paths_length_one(self):
+        scores = pesq(
+            [self.ref_path],
+            [self.deg_path]
+        )
+        nptest.assert_equal(scores, numpy.asarray([1.083]))
 
-    def test_wb_scores_thread(self):
-        scores = threaded_pesq(3*[self.ref], 3*[self.deg], 'wb', self.rate)
-        nptest.assert_equal(scores, numpy.asarray(3*[[0, 1.083]]))
+    def test_wb_scores_with_lists_of_paths_length_two(self):
+        scores = pesq(
+            [self.ref_path, self.ref_path],
+            [self.deg_path, self.ref_path]
+        )
+        nptest.assert_equal(scores, numpy.asarray([1.083, 4.644]))
 
-    def test_nb_scores_thread(self):
-        scores = threaded_pesq(3*[self.ref], 3*[self.deg], 'nb', self.rate)
-        nptest.assert_equal(scores, numpy.asarray(3*[[1.969, 1.607]]))
+    def test_wb_scores_with_lists_of_arrays_length_one(self):
+        scores = pesq(
+            [self.ref_array],
+            [self.deg_array]
+        )
+        nptest.assert_equal(scores, numpy.asarray([1.082]))  # Slightly off
+
+    def test_wb_scores_with_lists_of_arrays_length_two(self):
+        scores = pesq(
+            [self.ref_array, self.ref_array],
+            [self.deg_array, self.ref_array]
+        )
+        nptest.assert_equal(scores, numpy.asarray([1.082, 4.644]))
+
+    def test_nb_scores_with_lists_of_paths_length_one(self):
+        scores = pesq(
+            [self.ref_path],
+            [self.deg_path],
+            'nb'
+        )
+        nptest.assert_equal(scores, numpy.asarray([1.607]))
+
+    def test_nb_scores_with_lists_of_paths_length_two(self):
+        scores = pesq(
+            [self.ref_path, self.ref_path],
+            [self.deg_path, self.ref_path],
+            'nb'
+        )
+        nptest.assert_equal(scores, numpy.asarray([1.607, 4.549]))
+
+    def test_nb_scores_with_lists_of_arrays_length_one(self):
+        scores = pesq(
+            [self.ref_array],
+            [self.deg_array],
+            'nb'
+        )
+        nptest.assert_equal(scores, numpy.asarray([1.552]))  # Slightly off
+
+    def test_nb_scores_with_lists_of_arrays_length_two(self):
+        scores = pesq(
+            [self.ref_array, self.ref_array],
+            [self.deg_array, self.ref_array],
+            'nb'
+        )
+        nptest.assert_equal(scores, numpy.asarray([1.552, 4.549]))
+
+    def test_wb_scores_with_paths_directly(self):
+        scores = pesq(
+            self.ref_path,
+            self.deg_path
+        )
+        nptest.assert_equal(scores, numpy.asarray([1.083]))
