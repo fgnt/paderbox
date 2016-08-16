edfcc7721b49d7c6f08f85db0a2add12e810f536
Lukas Drude <mail@lukas-drude.de>
2016-08-16 15:53:39 +0200

    Added change_directory context manager and use this for building pesq

--- nt/utils/__init__.py
+++ nt/utils/__init__.py
@@ -1,5 +1,5 @@
 from .deprecated import deprecated
-from nt.utils.file_handling import *
+from nt.utils.file_handling import mkdir_p, change_directory
 from nt.utils.timer import Timer, timeStamped
 from nt.utils.container import Container, AttrDict
 from nt.utils.json_utils import *
@@ -7,3 +7,4 @@ from nt.utils.json_utils import *
 
 mkdir_p = mkdir_p
 Container = Container
+change_directory = change_directory
--- nt/utils/file_handling.py
+++ nt/utils/file_handling.py
@@ -1,5 +1,6 @@
 import os
 
+
 def mkdir_p(path):
     """ Creates a path recursively without throwing an error if it already exists
 
@@ -13,3 +14,19 @@ def mkdir_p(path):
     except FileNotFoundError:
         if path == '':
             pass
+
+
+class change_directory:
+    """Context manager for changing the current working directory
+
+    http://stackoverflow.com/a/13197763/911441
+    """
+    def __init__(self, new_path):
+        self.new_path = os.path.expanduser(new_path)
+
+    def __enter__(self):
+        self.saved_path = os.getcwd()
+        os.chdir(self.new_path)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.saved_path)
