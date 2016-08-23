abc4b540e9818a263e947d63ff82f1c34523e0ca
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2016-08-23 16:58:58 +0200

    add more asserts to tests_reshape

--- tests/utils_test/test_reshape.py
+++ tests/utils_test/test_reshape.py
@@ -4,7 +4,7 @@ import nt.testing as tc
 from nt.utils.numpy_utils import reshape
 
 
-T, B, F = 400, 6, 513
+T, B, F = 40, 6, 51
 A = np.random.uniform(size=(T, B, F))
 A2 = np.random.uniform(size=(T, 1, B, F))
 A3 = np.random.uniform(size=(T*B*F,))
@@ -13,31 +13,49 @@ A4 = np.random.uniform(size=(T, 1, 1, B, 1, F))
 
 class TestReshape(unittest.TestCase):
     def test_noop_comma(self):
-        tc.assert_equal(reshape(A, 'T,B,F->T,B,F').shape, (T, B, F))
+        result = reshape(A, 'T,B,F->T,B,F')
+        tc.assert_equal(result.shape, (T, B, F))
+        tc.assert_equal(result, A)
 
     def test_noop_space(self):
-        tc.assert_equal(reshape(A, 'T B F->T B F').shape, (T, B, F))
+        result = reshape(A, 'T B F->T B F')
+        tc.assert_equal(result.shape, (T, B, F))
+        tc.assert_equal(result, A)
 
     def test_noop_mixed(self):
-        tc.assert_equal(reshape(A, 'tbf->t, b f').shape, (T, B, F))
+        result = reshape(A, 'tbf->t, b f')
+        tc.assert_equal(result.shape, (T, B, F))
+        tc.assert_equal(result, A)
 
     def test_transpose_comma(self):
-        tc.assert_equal(reshape(A, 'T,B,F->F,T,B').shape, (F, T, B))
+        result = reshape(A, 'T,B,F->F,T,B')
+        tc.assert_equal(result.shape, (F, T, B))
+        tc.assert_equal(result, A.transpose(2, 0, 1))
 
     def test_transpose_mixed(self):
-        tc.assert_equal(reshape(A, 't, b, f -> f t b').shape, (F, T, B))
+        result = reshape(A, 't, b, f -> f t b')
+        tc.assert_equal(result.shape, (F, T, B))
+        tc.assert_equal(result, A.transpose(2, 0, 1))
 
     def test_broadcast_axis_0(self):
-        tc.assert_equal(reshape(A, 'T,B,F->1,T,B,F').shape, (1, T, B, F))
+        result = reshape(A, 'T,B,F->1,T,B,F')
+        tc.assert_equal(result.shape, (1, T, B, F))
+        tc.assert_equal(result, A[None, ...])
 
     def test_broadcast_axis_2(self):
-        tc.assert_equal(reshape(A, 'T,B,F->T,B,1,F').shape, (T, B, 1, F))
+        result = reshape(A, 'T,B,F->T,B,1,F')
+        tc.assert_equal(result.shape, (T, B, 1, F))
+        tc.assert_equal(result, A[..., None, :])
 
     def test_broadcast_axis_3(self):
-        tc.assert_equal(reshape(A, 'T,B,F->T,B,F,1').shape, (T, B, F, 1))
+        result = reshape(A, 'T,B,F->T,B,F,1')
+        tc.assert_equal(result.shape, (T, B, F, 1))
+        tc.assert_equal(result, A[..., None])
 
     def test_reshape_comma(self):
-        tc.assert_equal(reshape(A, 'T,B,F->T,B*F').shape, (T, B*F))
+        result = reshape(A, 'T,B,F->T,B*F')
+        tc.assert_equal(result.shape, (T, B*F))
+        tc.assert_equal(result, A.reshape(T, B*F))
 
     def test_reshape_comma_unflatten(self):
         with tc.assert_raises(NotImplementedError):
@@ -48,25 +66,37 @@ class TestReshape(unittest.TestCase):
             reshape(A3, 't*b*f->f, t*b')
 
     def test_reshape_comma_flat(self):
-        tc.assert_equal(reshape(A, 'T,B,F->T*B*F').shape, (T*B*F,))
+        result = reshape(A, 'T,B,F->T*B*F')
+        tc.assert_equal(result.shape, (T*B*F,))
+        tc.assert_equal(result, A.ravel())
 
     def test_reshape_comma_with_singleton_input(self):
-        tc.assert_equal(reshape(A2, 'T, 1, B, F -> T*B*F').shape, (T*B*F,))
+        result = reshape(A2, 'T, 1, B, F -> T*B*F')
+        tc.assert_equal(result.shape, (T*B*F,))
+        tc.assert_equal(result, A2.ravel())
 
     def test_reshape_comma_with_a_lot_of_singleton_inputs(self):
-        tc.assert_equal(reshape(A4, 'T, 1, 1, B, 1, F -> T*B*F').shape, (T*B*F,))
+        result = reshape(A4, 'T, 1, 1, B, 1, F -> T*B*F')
+        tc.assert_equal(result.shape, (T*B*F,))
+        tc.assert_equal(result, A4.ravel())
 
     def test_reshape_and_broadcast(self):
         tc.assert_equal(reshape(A, 'T,B,F->T,1,B*F').shape, (T, 1, B*F))
         tc.assert_equal(reshape(A, 'T,B,F->T,1,B*F').ravel(), A.ravel())
 
     def test_reshape_and_broadcast_many(self):
-        tc.assert_equal(reshape(A, 'T,B,F->1,T,1,B*F,1').shape, (1, T, 1, B*F, 1))
+        result = reshape(A, 'T,B,F->1,T,1,B*F,1')
+        tc.assert_equal(result.shape, (1, T, 1, B*F, 1))
+
+    def test_swap_and_reshape(self):
+        result = reshape(A, 'T,B,F->T,F*B')
+        tc.assert_equal(result.shape, (T, F * B))
+        tc.assert_equal(result, A.swapaxes(-1, -2).reshape(T, F * B))
 
     def test_transpose_and_reshape(self):
         result = reshape(A, 'T,B,F->F,B*T')
         tc.assert_equal(result.shape, (F, B*T))
-        tc.assert_almost_equal(result, A.transpose(2, 1, 0).reshape(F, B*T))
+        tc.assert_equal(result, A.transpose(2, 1, 0).reshape(F, B*T))
 
     def test_all_comma(self):
         tc.assert_equal(reshape(A, 'T,B,F->F,1,B*T').shape, (F, 1, B*T))
