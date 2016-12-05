db1d19a35a8a873c755d8c4cd5fcf7784884db1d
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2016-12-05 17:14:33 +0100

    matlab fix licence path

--- nt/utils/matlab.py
+++ nt/utils/matlab.py
@@ -28,8 +28,8 @@ class Mlab:
             mlab_process = Matlab(
                 'nice -n 3 ' +
                 '/net/ssd/software/MATLAB/R2015a/bin/matlab' +
-                ' -c /opt/MATLAB/license.dat'
-		# -c /opt/MATLAB/R2014a_studis/licenses/network.lic'
+                # ' -c /opt/MATLAB/license.dat'
+		        ' -c /opt/MATLAB/R2016b_studis/licenses/network.lic'
                 ' -nodisplay -nosplash'
             )
         else:
