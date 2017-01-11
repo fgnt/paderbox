94d16da5d9e84e456417af427fb59305f57f5494
Jahn Heymann <heymann@nt.upb.de>
2017-01-11 11:12:22 +0100

    Debug Jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -12,22 +12,6 @@ trap 'exit 0' EXIT SIGINT SIGTERM
 
 env
 
-# Set Paths
-CUDA_PATH=/usr/local/cuda
-LD_LIBRARY_PATH=$CUDA_PATH/lib64:${LD_LIBRARY_PATH}
-PATH=$CUDA_PATH/bin:$PATH
-PATH=/net/ssd/software/anaconda/bin/:$PATH
-source activate py35
-export PATH
-export LD_LIBRARY_PATH
-
-#echo 'PATH=/net/ssd/software/anaconda/bin/:$PATH' >> ~/.bashrc
-#echo "source activate py35" >> ~/.bashrc
-
-cat ~/.bashrc
-
-env
-
 # Use a pseudo virtualenv, http://stackoverflow.com/questions/2915471/install-a-python-package-into-a-different-directory-using-pip
 mkdir -p venv
 export PYTHONUSERBASE=$(readlink -m venv)
@@ -54,12 +38,20 @@ pip show chainer
 nosetests -a '!matlab' --with-xunit --with-coverage --cover-package=nt -v # --processes=-1
 # Use as many prosesses as you have cores: --processes=-1
 
+# Uninstall packages
+pip uninstall --quiet --yes chainer
+pip uninstall --quiet --yes nt
+
+unset $PYTHONUSERBASE
+
 # Export coverage
 python -m coverage xml --include=nt*
 
 # Pylint tests
 pylint --rcfile=pylint.cfg -f parseable nt > pylint.txt
 # --files-output=y is a bad option, because it produces hundreds of files
+
+env
 
 # Build documentation
 make --directory=doc/source/auto_reference/ clean
@@ -69,7 +61,3 @@ make --directory=doc html
 
 # Store pip packages
 pip freeze > pip.txt
-
-# Uninstall packages
-pip uninstall --quiet --yes chainer
-pip uninstall --quiet --yes nt
