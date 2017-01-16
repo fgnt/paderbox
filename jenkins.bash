8da83d1e72fd081d385a12e12ec085b613db392c
neumann <tvn@mail.upb.de>
2017-01-16 16:25:38 +0100

    Move common code of jenkins files into separate file

--- jenkins.bash
+++ jenkins.bash
@@ -1,39 +1,7 @@
 #!/usr/bin/env bash
 
-renice -n 20 $$
-
-# set a prefix for each cmd
-green='\033[0;32m'
-NC='\033[0m' # No Color
-trap 'echo -e "${green}$ $BASH_COMMAND ${NC}"' DEBUG
-
-# Force Exit 0
-trap 'exit 0' EXIT SIGINT SIGTERM
-
-# Use a pseudo virtualenv, http://stackoverflow.com/questions/2915471/install-a-python-package-into-a-different-directory-using-pip
-mkdir -p venv
-export PYTHONUSERBASE=$(readlink -m venv)
-
-TOOLBOX=./toolbox
-CHAINER=./chainer
-
-# Refresh files...
-ls /net/ssd/software/anaconda/envs/py35/lib/python3.5/lib-dynload/../../ > /dev/null
-
-# Refresh toolbox
-# TODO: install all dependencies and add --no-deps option
-pip uninstall --quiet --yes nt
-ls
-pip show nt
-pip install  --quiet --user -e ${TOOLBOX}
-pip show nt
-
-# Update chainer
-pip uninstall --quiet --yes chainer
-ls chainer
-pip show chainer
-pip install --quiet --user -e ${CHAINER}
-pip show chainer
+# include common stuff (installation of toolbox and chainer, paths, traps, nice level...)
+source ./jenkins_common.bash
 
 # Unittets
 # It seems, that jenkins currentliy does not work with matlab: Error: Segmentation violation
