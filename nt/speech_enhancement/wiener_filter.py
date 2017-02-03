7537e1d57f97777b0745efca63d6fa34286bb031
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2017-02-03 16:44:03 +0100

    corrected wiener_filter (recursion was missing)

--- nt/speech_enhancement/wiener_filter.py
+++ nt/speech_enhancement/wiener_filter.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def wiener_filter_gain(observation, n_mask, G_min_db=-25, mask_min=1e-6):
+def wiener_filter_gain_wrong(observation, n_mask, G_min_db=-25, mask_min=1e-6):
     """
         Wiener filter implementation as suggested in the DSSP script.
         This function returns the gain function, which is multiplied with the
@@ -34,7 +34,56 @@ def wiener_filter_gain(observation, n_mask, G_min_db=-25, mask_min=1e-6):
     Phi_NN_smoothed = Phi_NN
     Phi_NN_smoothed[1:, :] = (1 - n_mask[1:, :]) * Phi_NN[:-1, :] \
                              + Phi_NN[1:, :]
+    Phi_NN_smoothed = np.maximum(Phi_NN_smoothed, mask_min)
     a_posteriori_SNR = Phi_XX / Phi_NN_smoothed
+    gain = np.maximum((1 - 1 / a_posteriori_SNR), G_min)
+    return gain
+
+
+def wiener_filter_gain(observation, n_mask, G_min_db=-25, mask_min=1e-6):
+    """
+        Wiener filter implementation as suggested in the DSSP script.
+        This function returns the gain function, which is multiplied with the
+        STFT of the noisy obseration.
+
+        :param observation: Single channel complex STFT signal with
+            dimensions TxF.
+        :param n_mask:
+        :param G_min_db: Minimal Gain in dB. Defaults to -25.
+        :type G_min_db: int, optional
+        :return: Estimate for the Wiener filter gain TxF.
+
+
+        >>> obs = np.array([1, 1, 1, 1, 1])[:, None]
+        >>> mask = np.array([1, 1, 1, 1, 1])[:, None]
+        >>> G_min_db = -25
+        >>> (wiener_filter_gain(obs, mask) - 10 ** (G_min_db / 20)).ravel()
+        array([ 0.,  0.,  0.,  0.,  0.])
+        >>> mask = np.array([1, 0, 0, 0, 0])[:, None]
+        >>> (wiener_filter_gain(obs, mask) - 10 ** (G_min_db / 20)).ravel()
+        array([ 0.,  0.,  0.,  0.,  0.])
+        """
+
+    def spectrogram(x):
+        # return np.sum(x.astype(np.complex128).
+        # view(np.float64).resahpe(*x.shape, 2)**2, axis=-1)
+
+        return x.real ** 2 + x.imag ** 2
+
+    G_min = 10 ** (G_min_db / 20)
+    n_mask = np.clip(n_mask, mask_min, (1 - mask_min))
+    Phi_XX = spectrogram(observation)
+    Phi_NN = spectrogram(n_mask * observation)
+    Phi_NN_smoothed = Phi_NN
+    for t in range(1, n_mask.shape[0]):
+        Phi_NN_smoothed[t, :] = (1 - n_mask[t, :]) * Phi_NN_smoothed[t-1, :] \
+                             + Phi_NN[t, :]
+
+    # Phi_NN_smoothed[1:, :] = (1 - n_mask[1:, :]) * Phi_NN[:-1, :] \
+    #                          + Phi_NN[1:, :]
+    Phi_NN_smoothed = np.maximum(Phi_NN_smoothed, mask_min)
+    a_posteriori_SNR = Phi_XX / Phi_NN_smoothed
+    a_posteriori_SNR = np.maximum(a_posteriori_SNR, mask_min)
     gain = np.maximum((1 - 1 / a_posteriori_SNR), G_min)
     return gain
 
@@ -54,3 +103,24 @@ def wiener_filter(observation, n_mask, G_min_db=-25, mask_min=1e-6):
     gain = wiener_filter_gain(observation, n_mask, G_min_db=G_min_db,
                               mask_min=mask_min)
     return gain * observation
+
+if __name__ == '__main__':
+    import asn
+    from nt.speech_enhancement.mask_module import wiener_like_mask
+    from nt.transform.module_stft import stft_v2 as stft, istft_v2 as istft
+    from nt.io import play, audiowrite
+    from nt.speech_enhancement.noise import get_snr
+    from nt.evaluation import pesq
+
+    dataset = asn.database.chime.Dataset_dt05_simu()
+    data = dataset[0]
+    obs = stft(data.observed[4])
+    x = stft(data.X[4])
+    n = stft(data.N[4])
+    _, n_mask = wiener_like_mask([x, n])
+    s_hat = wiener_filter(obs, n_mask)
+    g = wiener_filter_gain(obs, n_mask)
+    # play.play(s_hat)
+    audiowrite.audiowrite(istft(s_hat), '/net/nas/boeddeker/asn_data/wiener_test.wav')
+    print('snr', get_snr(istft(x*g), istft(n*g)))
+    print('pesq', pesq.pesq(data.X[4], istft(s_hat)))
