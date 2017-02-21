50c1ab4f94e7639d4661c6461e1c93d496919659
Jahn Heymann <heymann@nt.upb.de>
2017-02-21 11:37:48 +0100

    Use db dir to define the database directories

--- nt/io/data_dir.py
+++ nt/io/data_dir.py
@@ -12,58 +12,22 @@
 import os
 from pathlib import Path
 
-ami = Path(os.getenv(
-    'NT_AMI_DIR',
-    '/net/db/ami'
+database_jsons = Path(os.getenv(
+    'NT_DATABASE_JSONS_DIR',
+    '/net/storage/database_jsons'
+))
+db_dir = Path(os.getenv(
+    'NT_DB_DIR',
+    '/net/db'
 ))
 testing = Path(os.getenv(
     'NT_TESTING_DIR',
     '/net/storage/python_unittest_data'
 ))
-timit = Path(os.getenv(
-    'NT_TIMIT_DIR',
-    '/net/db/timit'
-))
-tidigits = Path(os.getenv(
-    'NT_TIDIGITS_DIR',
-    '/net/db/tidigits'
-))
-database_jsons = Path(os.getenv(
-    'NT_DATABASE_JSONS_DIR',
-    '/net/storage/database_jsons'
-))
-chime_3 = Path(os.getenv(
-    'NT_CHIME_3_DIR',
-    '/net/db/chime3'
-))
-chime_4 = Path(os.getenv(
-    'NT_CHIME_4_DIR',
-    '/net/db/chime4'
-))
 kaldi_root = Path(os.getenv(
     'KALDI_ROOT',
     '/net/ssd/jheymann/software/kaldi_latest'
     # '/net/ssd/software/kaldi'
-))
-merl_mixtures = Path(os.getenv(
-    'NT_MERL_MIXTURES_DIR',
-    '/net/db/merl_speaker_mixtures'
-))
-german_speechdata = Path(os.getenv(
-    'NT_GERMAN_SPEECHDATA_DIR',
-    '/net/storage/jheymann/speech_db/german-speechdata-package-v2/'
-))
-noisex92 = Path(os.getenv(
-    'NT_NoiseX_92_DIR',
-    '/net/db/NoiseX_92'
-))
-wsj = Path(os.getenv(
-    'NT_WSJ_DIR',
-    '/net/db/wsj'
-))
-dcase = Path(os.getenv(
-    'NT_DCASE_DIR',
-    '/home/parora/Documents/DCASE/DCASE 2016/'
 ))
 matlab_toolbox = Path(os.getenv(
     'MATLAB_TOOLBOX_DIR',
@@ -76,6 +40,47 @@ matlab_r2015a = Path(os.getenv(
 matlab_license = Path(os.getenv(
     'MATLAB_LICENSE',
     '/opt/MATLAB/R2016b_studis/licenses/network.lic'
+))
+
+ami = Path(os.getenv(
+    'NT_AMI_DIR',
+    db_dir / 'ami'
+))
+timit = Path(os.getenv(
+    'NT_TIMIT_DIR',
+    '/net/db/timit'
+))
+tidigits = Path(os.getenv(
+    'NT_TIDIGITS_DIR',
+    '/net/db/tidigits'
+))
+chime_3 = Path(os.getenv(
+    'NT_CHIME_3_DIR',
+    db_dir / 'chime3'
+))
+chime_4 = Path(os.getenv(
+    'NT_CHIME_4_DIR',
+    db_dir / 'chime4'
+))
+merl_mixtures = Path(os.getenv(
+    'NT_MERL_MIXTURES_DIR',
+    '/net/db/merl_speaker_mixtures'
+))
+german_speechdata = Path(os.getenv(
+    'NT_GERMAN_SPEECHDATA_DIR',
+    '/net/storage/jheymann/speech_db/german-speechdata-package-v2/'
+))
+noisex92 = Path(os.getenv(
+    'NT_NoiseX_92_DIR',
+    db_dir / 'NoiseX_92'
+))
+wsj = Path(os.getenv(
+    'NT_WSJ_DIR',
+    db_dir / 'wsj'
+))
+dcase = Path(os.getenv(
+    'NT_DCASE_DIR',
+    '/home/parora/Documents/DCASE/DCASE 2016/'
 ))
 language_model = Path(os.getenv(
     'LANGUAGE_MODEL',
