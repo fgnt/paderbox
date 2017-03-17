e7f6d0d87e46ae7699c2fd45f79da3e1cc1b7864
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2017-03-17 14:07:48 +0100

    nt.io.data_dir: use expanduser

--- nt/io/data_dir.py
+++ nt/io/data_dir.py
@@ -12,88 +12,92 @@
 import os
 from pathlib import Path
 
-database_jsons = Path(os.getenv(
+
+def _get_path(environment_name, default):
+    return Path(os.getenv(environment_name, default)).expanduser()
+
+database_jsons = _get_path(
     'NT_DATABASE_JSONS_DIR',
     '/net/storage/database_jsons'
-))
-db_dir = Path(os.getenv(
+)
+db_dir = _get_path(
     'NT_DB_DIR',
     '/net/db'
-))
-testing = Path(os.getenv(
+)
+testing = _get_path(
     'NT_TESTING_DIR',
     '/net/storage/python_unittest_data'
-))
-kaldi_root = Path(os.getenv(
+)
+kaldi_root = _get_path(
     'KALDI_ROOT',
     '/net/ssd/jheymann/software/kaldi_latest'
     # '/net/ssd/software/kaldi'
-))
-matlab_toolbox = Path(os.getenv(
+)
+matlab_toolbox = _get_path(
     'MATLAB_TOOLBOX_DIR',
     '/net/ssd/software/matlab_toolbox'
-))
-matlab_r2015a = Path(os.getenv(
+)
+matlab_r2015a = _get_path(
     'MATLAB_R2015a',
     '/net/ssd/software/MATLAB/R2015a'
-))
-matlab_license = Path(os.getenv(
+)
+matlab_license = _get_path(
     'MATLAB_LICENSE',
     '/opt/MATLAB/R2016b_studis/licenses/network.lic'
-))
+)
 
-ami = Path(os.getenv(
+ami = _get_path(
     'NT_AMI_DIR',
     db_dir / 'ami'
-))
-timit = Path(os.getenv(
+)
+timit = _get_path(
     'NT_TIMIT_DIR',
     db_dir /'timit'
-))
-tidigits = Path(os.getenv(
+)
+tidigits = _get_path(
     'NT_TIDIGITS_DIR',
     db_dir /'tidigits'
-))
-chime_3 = Path(os.getenv(
+)
+chime_3 = _get_path(
     'NT_CHIME_3_DIR',
     db_dir / 'chime3'
-))
-chime_4 = Path(os.getenv(
+)
+chime_4 = _get_path(
     'NT_CHIME_4_DIR',
     db_dir / 'chime4'
-))
-merl_mixtures = Path(os.getenv(
+)
+merl_mixtures = _get_path(
     'NT_MERL_MIXTURES_DIR',
     '/net/db/merl_speaker_mixtures'
-))
-german_speechdata = Path(os.getenv(
+)
+german_speechdata = _get_path(
     'NT_GERMAN_SPEECHDATA_DIR',
-    '/net/db/german-speechdata-package-v2/'
-))
-noisex92 = Path(os.getenv(
+    '/net/storage/jheymann/speech_db/german-speechdata-package-v2/'
+)
+noisex92 = _get_path(
     'NT_NoiseX_92_DIR',
     db_dir / 'NoiseX_92'
-))
-reverb = Path(os.getenv(
+)
+reverb = _get_path(
     'NT_REVERB_DIR',
     db_dir / 'reverb'
-))
-wsj = Path(os.getenv(
+)
+wsj = _get_path(
     'NT_WSJ_DIR',
     db_dir / 'wsj'
-))
-dcase = Path(os.getenv(
+)
+dcase = _get_path(
     'NT_DCASE_DIR',
     '/home/parora/Documents/DCASE/DCASE 2016/'
-))
-wsjcam0 = Path(os.getenv(
+)
+wsjcam0 = _get_path(
     'NT_WSJCAM0_DIR',
     db_dir / 'wsjcam0'
-))
-language_model = Path(os.getenv(
+)
+language_model = _get_path(
     'LANGUAGE_MODEL',
     '/net/storage/jheymann/__share/ldrude/2016/2016-05-10_lm'
-))
+)
 
 if __name__ == "__main__":
     import doctest
