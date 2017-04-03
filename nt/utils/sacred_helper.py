c5bea24274f02a34ca2b35295519352a723e7e2f
Lukas Drude <mail@lukas-drude.de>
2017-04-03 17:52:03 +0200

    Uses new interface for JsonObserver with template strings.

--- nt/utils/sacred_helper.py
+++ nt/utils/sacred_helper.py
@@ -15,23 +15,24 @@ class LocalExperiment(Experiment):
         """Simplifies experiment creation. Automatically adds date prefix.
 
         Assumes, that you have a data root which may contain more than one
-        exeperiment.
+        experiment.
+        If you need a more generic interface, use Sacred directly.
 
         Args:
-            data_root: Path object, i.e. Path('/net/vol/project/data')
+            data_dir: Can be a format string or a Path object.
+                Possible parts are {id}, {now} and {name}, i.e.
+                'data_root/{name}/{now}_{id}'.
             experiment_name: String, i.e. 'enhancement'
         Returns:
             Experiment to be used as decorator in main Sacred file.
         """
         super(LocalExperiment, self).__init__(experiment_name)
-        self.observers.append(JsonObserver.create(
-            data_dir,
-        ))
+        self.observers.append(JsonObserver.create(data_dir=data_dir))
 
     @property
-    def target_path(self):
+    def data_dir(self):
         """Short name to get target folder (use in main function)."""
-        return self.observers[0].folder
+        return self.observers[0].data_dir
 
 
 def get_path_by_id(
