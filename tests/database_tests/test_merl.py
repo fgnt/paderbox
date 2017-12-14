f1fd8c6547a8903384949504ea18f41df2113d7f
danielha <daniel.hasenklever@gmx.de>
2017-12-14 18:58:28 +0100

    db tests for new json

--- tests/database_tests/test_merl.py
+++ tests/database_tests/test_merl.py
@@ -5,30 +5,31 @@ from nt.testing import db_test
 from nt.io import load_json
 from nt.database.keys import *
 
-ger_json = db_test.ROOT / "german.json"
+merl = db_test.ROOT / "merl_speaker_mixtures_min_8k.json"
+merl = '/home/danielha/Schreibtisch/merl.json'
 
 class test_ger_db(db_test.DatabaseTest):
 
     def setUp(self):
-        self.json = load_json(ger_json)
+        self.json = load_json(merl)
 
     def test_structure(self):
         self.assertIn(DATASETS, self.json)
         self.assertIn(EXAMPLES, self.json)
-        self.assertIn('dev_Kinect-Beam', self.json[DATASETS])
+        self.assertIn('mix_2_spk_min_cv', self.json[DATASETS])
 
     def test_len(self):
         # ids
-        self.assertEqual(len(list(self.json[EXAMPLES])), 14907,
-                         "There should be 14907 utt_ids in '{}'!"
+        self.assertEqual(len(list(self.json[EXAMPLES])), 56000,
+                         "There should be 56000 utt_ids in '{}'!"
                          .format(EXAMPLES))
         # datasets
-        self.assertEqual(len(list(self.json[DATASETS])), 15,
-                         "There should be 15 datasets in '{}'!"
+        self.assertEqual(len(list(self.json[DATASETS])), 6,
+                         "There should be 6 datasets in '{}'!"
                          .format(DATASETS))
         # dataset length
-        self.assertEqual(len(list(self.json[DATASETS]['dev_Kinect-Beam'])), 1084,
-                    "There should be 1084 utt_ids in dataset 'dev_Kinect-Beam'!")
+        self.assertEqual(len(list(self.json[DATASETS]['mix_2_spk_min_cv'])), 5000,
+                    "There should be 5000 utt_ids in dataset 'mix_2_spk_min_cv'!")
 
     def test_examples(self):
         utt_id = list(self.json[EXAMPLES])[0]
--- tests/database_tests/test_reverb.py
+++ tests/database_tests/test_reverb.py
@@ -5,30 +5,31 @@ from nt.testing import db_test
 from nt.io import load_json
 from nt.database.keys import *
 
-ger_json = db_test.ROOT / "german.json"
+reverb = db_test.ROOT / "reverb.json"
+reverb = '/home/danielha/Schreibtisch/reverb.json'
 
-class test_ger_db(db_test.DatabaseTest):
+class test_reverb(db_test.DatabaseTest):
 
     def setUp(self):
-        self.json = load_json(ger_json)
+        self.json = load_json(reverb)
 
     def test_structure(self):
         self.assertIn(DATASETS, self.json)
         self.assertIn(EXAMPLES, self.json)
-        self.assertIn('dev_Kinect-Beam', self.json[DATASETS])
+        self.assertIn('RealData_dt_for_1ch_far_room1', self.json[DATASETS])
 
     def test_len(self):
         # ids
-        self.assertEqual(len(list(self.json[EXAMPLES])), 14907,
-                         "There should be 14907 utt_ids in '{}'!"
+        self.assertEqual(len(list(self.json[EXAMPLES])), 36216,
+                         "There should be 36216 utt_ids in '{}'!"
                          .format(EXAMPLES))
         # datasets
-        self.assertEqual(len(list(self.json[DATASETS])), 15,
-                         "There should be 15 datasets in '{}'!"
+        self.assertEqual(len(list(self.json[DATASETS])), 51,
+                         "There should be 52 datasets in '{}'!"
                          .format(DATASETS))
         # dataset length
-        self.assertEqual(len(list(self.json[DATASETS]['dev_Kinect-Beam'])), 1084,
-                    "There should be 1084 utt_ids in dataset 'dev_Kinect-Beam'!")
+        self.assertEqual(len(list(self.json[DATASETS]['RealData_dt_for_1ch_far_room1'])), 89,
+                    "There should be 89 utt_ids in dataset 'RealData_dt_for_1ch_far_room1'!")
 
     def test_examples(self):
         utt_id = list(self.json[EXAMPLES])[0]
