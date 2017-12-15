130182269bca8893ccbad88162006d78b43c8772
Lukas Drude <mail@lukas-drude.de>
2017-12-15 13:27:05 +0100

    Code Review: test_merl.py

--- tests/database_tests/test_merl.py
+++ tests/database_tests/test_merl.py
@@ -1,14 +1,13 @@
-import json
 import unittest
 
-from nt.testing import db_test
-from nt.io import load_json
 from nt.database.keys import *
+from nt.io import load_json
+from nt.testing import db_test
 
 merl = db_test.ROOT / "merl_speaker_mixtures_min_8k.json"
 
-class test_ger_db(db_test.DatabaseTest):
 
+class TestMerlDatabase(db_test.DatabaseTest):
     def setUp(self):
         self.json = load_json(merl)
 
@@ -18,17 +17,26 @@ class test_ger_db(db_test.DatabaseTest):
         self.assertIn('mix_2_spk_min_cv', self.json[DATASETS])
 
     def test_len(self):
-        # ids
-        self.assertEqual(len(list(self.json[EXAMPLES])), 56000,
-                         "There should be 56000 utt_ids in '{}'!"
-                         .format(EXAMPLES))
-        # datasets
-        self.assertEqual(len(list(self.json[DATASETS])), 6,
-                         "There should be 6 datasets in '{}'!"
-                         .format(DATASETS))
-        # dataset length
-        self.assertEqual(len(list(self.json[DATASETS]['mix_2_spk_min_cv'])), 5000,
-                    "There should be 5000 utt_ids in dataset 'mix_2_spk_min_cv'!")
+        ids = 56000
+        self.assertEqual(
+            len(list(self.json[EXAMPLES])),
+            ids,
+            f"There should be {ids} utt_ids in '{EXAMPLES}'!"
+        )
+        datasets = 6
+        self.assertEqual(
+            len(list(self.json[DATASETS])),
+            datasets,
+            f"There should be {datasets} datasets in '{DATASETS}'!"
+        )
+        dataset_length = dict(
+            mix_2_spk_min_cv=5000
+        )
+        for k, v in dataset_length:
+            self.assertEqual(
+                len(list(self.json[DATASETS][k])),
+                v, f"There should be {v} utt_ids in dataset '{k}'!"
+            )
 
     def test_examples(self):
         utt_id = list(self.json[EXAMPLES])[0]
@@ -36,6 +44,7 @@ class test_ger_db(db_test.DatabaseTest):
         self.assertIn(AUDIO_PATH, self.json[EXAMPLES][utt_id])
         # transcription
         self.assertIn(TRANSCRIPTION, self.json[EXAMPLES][utt_id])
+
 
 if __name__ == '__main__':
     unittest.main()
