8d5d47547dc781ceb35d147d10c9748d026d6e65
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2018-04-27 14:17:49 +0200

    next try of matplotlib fix

--- nt/visualization/__init__.py
+++ nt/visualization/__init__.py
@@ -1,10 +1,10 @@
+from . import matplotlib_fix
+
 from nt.visualization.module_facet_grid import facet_grid, facet_grid_zero_space_time_frequency_plot
 from nt.visualization.plot import time_series
 from nt.visualization.context_manager import context_manager
 from nt.visualization.context_manager import LatexContextManager
 from nt.visualization.display_pdf import PDF
-
-from . import matplotlib_fix
 
 facet_grid = facet_grid
 time_series = time_series
