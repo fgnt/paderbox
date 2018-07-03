2862c7fa23194e7ee49c684c4d5c197ac9fa9643
Lukas Drude <mail@lukas-drude.de>
2018-07-03 19:14:22 +0200

    Fix ImportError when using no mkl_fft.

--- nt/__init__.py
+++ nt/__init__.py
@@ -1,5 +1,8 @@
-from numpy.fft import restore_all as _restore_all
-# https://github.com/numpy/numpy/issues/11456
-# https://github.com/ContinuumIO/anaconda-issues/issues/9697
-# https://github.com/IntelPython/mkl_fft/issues/11
-_restore_all()
+try:
+    from numpy.fft import restore_all as _restore_all
+    # https://github.com/numpy/numpy/issues/11456
+    # https://github.com/ContinuumIO/anaconda-issues/issues/9697
+    # https://github.com/IntelPython/mkl_fft/issues/11
+    _restore_all()
+except ImportError:
+    print('You do not use mkl_fft. We do not need to restore anything.')
