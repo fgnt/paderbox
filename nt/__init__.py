7534382249ee0cc20085f19daab1f8e3f5094a79
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2018-07-03 15:04:08 +0200

    fix __init__ to allow 'python -c "import numpy"' (Name collision with "nt" package)

--- nt/__init__.py
+++ nt/__init__.py
@@ -1,5 +1,5 @@
-import numpy as np
+from numpy.fft import restore_all as _restore_all
 # https://github.com/numpy/numpy/issues/11456
 # https://github.com/ContinuumIO/anaconda-issues/issues/9697
 # https://github.com/IntelPython/mkl_fft/issues/11
-np.fft.restore_all()
+_restore_all()
