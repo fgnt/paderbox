a3bbfc06fc17fe1214c66a4a47b82bf1bc13fac0
jensheit <heitkaemper@nt.upb.de>
2018-07-09 15:36:24 +0200

    added decoding to kaldi_array

--- chime5/scripts/kaldi_array.py
+++ chime5/scripts/kaldi_array.py
@@ -3,45 +3,22 @@ import tqdm
 from shutil import copytree, copyfile
 from nt.utils.process_caller import run_process
 import os
+from functools import partial
 import stat
 import sacred
 import gzip
 from nt.database import JsonDatabase, keys
 from chime5.scripts.kaldi import check_dest_dir, ORG_DIR
 from nt.database.chime5 import Chime5, adjust_start_end
-from nt.io.json_module import load_json
-
-mapping = load_json('/net/vol/jensheit/chime5_playground/data/mapping_new.json')
-
-
-chime5_db = Chime5()
-chime5_it = chime5_db.get_iterator_by_names('train')
-
-ex = sacred.Experiment('Chime5 train HMM GMM on array')
-
-class MapFunKaldiInear2Array:
-    def __init__(self, num_arrays=6, num_channels=4):
-        self.num_arrays = num_arrays
-        self.num_channels = num_channels
-        id_list = [ids for ids in mapping.values()]
-        self.map_dict = {self.get_inear_ids(ids, 'left'): self.get_ids(ids)
-                         for ids in id_list}
-
-    def get_inear_ids(self, id_dict, site):
-        return id_dict['worn_microphone'][site]
-
-
-    def get_ids(self, id_dict):
-        return  (array[channel] for channel in range(self.num_channels)
-                 for array in id_dict[keys.OBSERVATION].values()
-                 if len(array)==4)
-
-    def __call__(self, kaldi_id):
-        return self.map_dict[kaldi_id]
-
+from chime5.scripts.create_mapping_json import Chime5KaldiIdMapping
+import codecs
+ex = sacred.Experiment('Kaldi array')
+db = Chime5()
+mapping = Chime5KaldiIdMapping()
+chime5_it = db.get_iterator_by_names('train')
 
 def multiply_alignments(org_ali_dir, out_ali_dir, utt2split_dict,
-                        id_map_fun: MapFunKaldiInear2Array(), num_jobs=16):
+                        id_map_fun: mapping.get_array_id_from_inear_id, num_jobs=16):
     assert not out_ali_dir.exists() or len(list(org_ali_dir.glob('*.gz'))) == 0,\
         f'The destination dir {out_ali_dir} already exists, please choose a' \
         f' different dir'
@@ -54,6 +31,7 @@ def multiply_alignments(org_ali_dir, out_ali_dir, utt2split_dict,
 
     def read_alignments(file):
         ali_decompressed = gzip.decompress(file.read_bytes())
+        # ali_decompressed = codecs.getreader('utf-8')(ali_decompressed)
         split_dict = {str(idx): list() for idx in range(1, num_jobs + 1)}
         for ex in ali_decompressed.decode().split('\n'):
             if ex == '':
@@ -64,6 +42,7 @@ def multiply_alignments(org_ali_dir, out_ali_dir, utt2split_dict,
             for utt in new_utts:
                 if utt in utt2split_dict:
                     split_dict[utt2split_dict[utt]].append(f'{utt} {utt_ali}')
+                    del utt2split_dict[utt]
         return split_dict
 
     global_split_dict = {str(idx): list() for idx in range(1, num_jobs + 1)}
@@ -159,8 +138,8 @@ def train_hmm_array(base_dir: Path, train_set='train_uall',
         calculate_mfccs(base_dir, train_set)
     if not (train_dir / f'split{num_jobs}').exists():
         run_process([
-            f'{base_dir}/utils/split_data.sh.sh',
-            f'{train_dir}', 'num_jobs'],
+            f'{base_dir}/utils/split_data.sh',
+            f'{train_dir}', f'{num_jobs}'],
             cwd=str(base_dir), stdout=None, stderr=None)
     if not out_ali_dir.exists() or len(list(out_ali_dir.glob('*.gz'))) == 0:
         org_ali_dir = base_dir / 'exp' / f'{org_hmm}_{org_train_set}_ali'
@@ -171,7 +150,7 @@ def train_hmm_array(base_dir: Path, train_set='train_uall',
                                  org_dir, org_ali_dir, num_jobs)
         assert len(list(org_ali_dir.glob('*.gz'))) != 0, f'No alignments found' \
                                                          f'in org_dir {org_ali_dir}'
-        map_fun = MapFunKaldiInear2Array(num_arrays, num_channels)
+        map_fun = partial(mapping.get_array_id_from_inear_id, arrays=num_arrays, channels=num_channels)
         utt2split_dict = get_utt2split_dict(train_dir, num_jobs)
         multiply_alignments(org_ali_dir, out_ali_dir, utt2split_dict, map_fun)
     else:
@@ -203,16 +182,6 @@ def get_utt2split_dict(train_dir, num_jobs=16):
 
 def fix_segment_file(train_set):
     print('fix the segment file ')
-    def invert(iterator, key_list):
-        def get_value(example):
-            out = example
-            for key in key_list:
-                out = out[key]
-            return out
-
-        values = [get_value(iterator[key]) for key in iterator.keys()]
-        return {out: key for out_dict, key in zip(values, iterator.keys())
-                for out_list in out_dict.values() for out in out_list}
 
     segment_file = train_set / 'segments'
     assert segment_file.exists(), f'No segments file in {train_set}, please' \
@@ -221,7 +190,7 @@ def fix_segment_file(train_set):
     segment_file.rename(segment_zw)
 
     it_mapped = chime5_it.map(adjust_start_end)
-    mapping_inv = invert(mapping, ['observation'])
+    mapping_inv = mapping.get_inverted_mapping_array()
     segment_list = []
     for line in segment_zw.open().readlines():
         example_id, info, start, end = line.split(' ')
@@ -266,11 +235,12 @@ def copy_ref_dir(dev_dir, ref_dev_dir, audio_dir):
     copytree(str(ref_dev_dir), str(dev_dir))
     (dev_dir / 'segments').unlink()
     (dev_dir / 'wav.scp').unlink()
-    ids = {wav_file: mapping[wav_file.name.split('.')[0]][keys.OBSERVATION]
+    ids = {wav_file: mapping.get_array_ids_from_nt_id(wav_file.name.split('.')[0], channels='ENH')
            for wav_file in audio_dir.glob('*')}
+    print(list(ids.values())[0])
     used_ids = {kaldi_id: wav_file for wav_file, kaldi_ids in ids.items()
-                for array, kaldi_id in kaldi_ids.items()
-                if 'enh' in array and kaldi_id in ref_ids}
+                for kaldi_id in kaldi_ids if kaldi_id in ref_ids}
+    assert len(used_ids) > 0
     if len(used_ids) < len(ids):
         print(f'Not all files in {audio_dir} were used, '
               f'{len(ids)-len(used_ids)} ids are not used in kaldi')
@@ -280,15 +250,15 @@ def copy_ref_dir(dev_dir, ref_dev_dir, audio_dir):
 
 def get_dev_dir(dest_dir: Path, enh='bss_beam', hires=True,
                 ref_dev_dir='dev_beamformit_ref', audio_dir=None, num_jobs=8):
-    if 'hires' in enh and hires:
+    if isinstance(enh, Path):
+        dev_dir = enh
+    elif 'hires' in enh and hires:
         dev_dir = dest_dir / 'data' / f'dev_{enh}'
-        config = 'mfcc_hires.conf'
     elif hires:
         dev_dir = dest_dir / 'data' / f'dev_{enh}_hires'
-        config = 'mfcc_hires.conf'
     else:
         dev_dir = dest_dir / 'data' / f'dev_{enh}'
-        config = 'mfcc.conf'
+    config = 'mfcc_hires.conf' if hires else 'mfcc.conf'
     if not dev_dir.exists():
         print(f'Directory {dev_dir} not found creating data directory')
         if isinstance(ref_dev_dir, str):
@@ -342,8 +312,8 @@ def default():
     org_hmm = 'tri3_worn'
     org_train_set = 'train_worn'
     org_dir = ORG_DIR
-    num_arrays = 6
-    num_channels = 4
+    num_arrays = None
+    num_channels = None
     decode = False
     model_dir = 'chain_train_worn_u100k_cleaned'
     audio_dir = None
