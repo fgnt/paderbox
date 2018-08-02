0f6d358fda4981f513a03efb77c70b74a56277fd
Lukas Drude <mail@lukas-drude.de>
2018-08-02 19:13:48 +0200

    Tests for Kaldi read/write keyed text file

---
+++ tests/kaldi_tests/test_io.py
@@ -0,0 +1,25 @@
+import unittest
+from nt.kaldi.io import read_keyed_text_file, write_keyed_text_file
+import tempfile
+from pathlib import Path
+
+
+class TestReadWriteKeyedTextFile(unittest.TestCase):
+    @staticmethod
+    def check(data_dict, as_list: bool):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            temp_file = Path(temp_dir) / 'text'
+            write_keyed_text_file(temp_file, data_dict, from_list=as_list)
+            result = read_keyed_text_file(temp_file, to_list=as_list)
+        assert isinstance(result, dict), type(result)
+        assert data_dict.keys() == result.keys(), (data_dict, result)
+        for k in data_dict.keys():
+            assert data_dict[k] == result[k], (k, data_dict, result)
+
+    def test_with_value_as_list_of_strings(self):
+        data_dict = dict(a=['a', '1'], b=['b', '2'], c=['c', '3'])
+        self.check(data_dict, as_list=True)
+
+    def test_with_value_as_single_string(self):
+        data_dict = dict(a='a 1', b='c 2', c='c 3')
+        self.check(data_dict, as_list=False)
