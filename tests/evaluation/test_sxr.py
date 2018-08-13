b4fbf5defc39a42dc8d4c93b55c6b7b173ea4f8c
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2018-08-13 16:49:40 +0200

    input/outout sxr breaking change: changed the input shape from matlab style to numpy style Introduce namedtuple for sxr return value Add asserts to break old code

--- tests/evaluation/test_sxr.py
+++ tests/evaluation/test_sxr.py
@@ -17,23 +17,22 @@ class TestSXR(unittest.TestCase):
         self.n /= np.sqrt(np.mean(self.n ** 2))
 
     def test_input_sxr(self):
-        sdr, sir, snr = input_sxr(
-            morph('kt->t1k', 10 * np.stack((self.s1, self.s2))),
-            morph('t->t1', self.n),
-            average_sources=False
-        )
+        images = morph('kt->k1t', 10 * np.stack((self.s1, self.s2)))
+        noise = morph('t->1t', self.n)
+        sdr, sir, snr = input_sxr(images, noise, average_sources=False)
+        assert all(sir == input_sxr(images, noise, average_sources=False).sir)
         np.testing.assert_allclose(sdr, 2 * [10 * np.log10(100/101)], atol=1e-6)
         np.testing.assert_allclose(sir, 2 * [0], atol=1e-6)
         np.testing.assert_allclose(snr, 2 * [20], atol=1e-6)
 
     def test_output_sxr_more_outputs_than_sources_inf(self):
         sdr, sir, snr = output_sxr(
-            morph('kKt->tkK', [
+            morph('kKt->kKt', [
                 [1 * self.s1, 0 * self.s2, 0 * self.n],
                 [0 * self.s1, 1 * self.s2, 0 * self.n]
             ]),
-            morph('Kt->tK', [0 * self.n, 0 * self.n, 1 * self.n]),
-            average_sources=False
+            morph('Kt->Kt', [0 * self.n, 0 * self.n, 1 * self.n]),
+            average_sources=False,
         )
         np.testing.assert_allclose(sdr, 2 * [np.inf])
         np.testing.assert_allclose(sir, 2 * [np.inf])
@@ -41,26 +40,33 @@ class TestSXR(unittest.TestCase):
 
     def test_output_sxr_more_outputs_than_sources(self):
         sdr, sir, snr = output_sxr(
-            morph('kKt->tkK', [
+            morph('kKt->kKt', [
                 [10 * self.s1, 1 * self.s2, 0 * self.n],
                 [0 * self.s1, 10 * self.s2, 0 * self.n]
             ]),
-            morph('Kt->tK', [10 * self.n, 0 * self.n, 0 * self.n]),
-            average_sources=False
+            morph('Kt->Kt', [10 * self.n, 0 * self.n, 0 * self.n]),
+            average_sources=False,
         )
         np.testing.assert_allclose(sdr, [0, 20], atol=1e-6)
         np.testing.assert_allclose(sir, [np.inf, 20], atol=1e-6)
         np.testing.assert_allclose(snr, [0, np.inf], atol=1e-6)
 
     def test_output_sxr(self):
-        sdr, sir, snr = output_sxr(
-            morph('kKt->tkK', [
+        image_contribution = morph('kKt->kKt', [
                 [10 * self.s1, 1 * self.s2],
                 [0 * self.s1, 10 * self.s2]
-            ]),
-            morph('Kt->tK', [10 * self.n, 0 * self.n]),
-            average_sources=False
+            ])
+        noise_contribution = morph('Kt->Kt', [10 * self.n, 0 * self.n])
+        sdr, sir, snr = output_sxr(
+            image_contribution,
+            noise_contribution,
+            average_sources=False,
         )
+        assert all(snr == output_sxr(
+            image_contribution,
+            noise_contribution,
+            average_sources=False,
+        ).snr)
         np.testing.assert_allclose(sdr, [0, 20], atol=1e-6)
         np.testing.assert_allclose(sir, [np.inf, 20], atol=1e-6)
         np.testing.assert_allclose(snr, [0, np.inf], atol=1e-6)
