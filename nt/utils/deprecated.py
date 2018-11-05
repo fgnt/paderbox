1b581640d942ab13c8d58ca206add95c4459404a
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2018-11-05 14:54:21 +0100

    deprecated decorator: only warn once

--- nt/utils/deprecated.py
+++ nt/utils/deprecated.py
@@ -23,26 +23,32 @@ def deprecated(instructions):
         """This is a decorator which can be used to mark functions
         as deprecated. It will result in a warning being emitted
         when the function is used."""
+
+        already_warned = False
+
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            message = 'Call to deprecated function {} ({}). {}'.format(
-                func.__qualname__,
-                inspect.getfile(func),
-                instructions)
+            nonlocal already_warned
+            if not already_warned:
+                already_warned = True
+                message = 'Call to deprecated function {} ({}). {}'.format(
+                    func.__qualname__,
+                    inspect.getfile(func),
+                    instructions)
 
-            # fileno = func.__code__.co_firstlineno if isinstance(func, types.FunctionType) else -1
-            #
-            # warnings.warn_explicit(message,
-            #                        category=DeprecatedWarning,
-            #                        filename=inspect.getfile(func),
-            #                        lineno=fileno)
+                # fileno = func.__code__.co_firstlineno if isinstance(func, types.FunctionType) else -1
+                #
+                # warnings.warn_explicit(message,
+                #                        category=DeprecatedWarning,
+                #                        filename=inspect.getfile(func),
+                #                        lineno=fileno)
 
-            frame = inspect.currentframe().f_back
+                frame = inspect.currentframe().f_back
 
-            warnings.warn_explicit(message,
-                                   category=DeprecatedWarning,
-                                   filename=inspect.getfile(frame.f_code),
-                                   lineno=frame.f_lineno)
+                warnings.warn_explicit(message,
+                                       category=DeprecatedWarning,
+                                       filename=inspect.getfile(frame.f_code),
+                                       lineno=frame.f_lineno)
 
             return func(*args, **kwargs)
 
