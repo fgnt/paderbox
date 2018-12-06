1e4125a018cd6c32bd2047c5fe04434e51049632
Jahn Heymann <heymann@nt.upb.de>
2018-12-06 13:49:08 +0100

    Adds Chime+Reverb and common map functions

--- nt/notebook.py
+++ nt/notebook.py
@@ -5,6 +5,7 @@ import itertools
 import os
 import re
 from collections import defaultdict
+from datetime import datetime
 from pathlib import Path
 from pprint import pprint as original_pprint
 
@@ -14,20 +15,21 @@ import pandas as pd
 import seaborn as sns
 from IPython.display import HTML
 from IPython.display import display
-from tqdm import tqdm
-from datetime import datetime
-
-from nt.io import load_json
+from nt.database.chime import Chime3, Chime4
+from nt.database.reverb import Reverb
 from nt.io import dump_json
+from nt.io import load_json
 from nt.io.play import play
 from nt.transform import fbank
 from nt.transform import istft
 from nt.transform import stft
+from nt.utils.numpy_utils import morph
+from nt.utils.pandas_helper import py_query
 from nt.visualization import context_manager
 from nt.visualization import facet_grid
 from nt.visualization import plot
-from nt.utils.numpy_utils import morph
-from nt.utils.pandas_helper import py_query
+from tqdm import tqdm
+from nt.database.iterator import AudioReader, LimitAudioLength, AlignmentReader
 
 __all__ = [
     "pprint",
@@ -54,7 +56,13 @@ __all__ = [
     "morph",
     "np",
     "datetime",
-    "py_query"
+    "py_query",
+    "Chime3",
+    "Chime4",
+    "Reverb",
+    "AudioReader",
+    "LimitAudioLength",
+    "AlignmentReader"
 ]
 
 
