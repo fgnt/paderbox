26dba06d3e289a3874cf7d54baeda6876783c50b
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2018-12-20 09:43:12 +0100

    Dispatcher: add close matches (similarity sorted)

--- paderbox/utils/mapping.py
+++ paderbox/utils/mapping.py
@@ -1,11 +1,33 @@
-class Dispatcher(dict):
-    """Is basically a dict with a better error message on key error."""
 
-    # TODO: Inherit from KeyError a DispatchError with lazy word comparison
+
+class DispatchError(KeyError):
+    def __str__(self):
+        if len(self.args) == 2:
+            item, keys = self.args
+            import difflib
+            # suggestions are sorted (similarity)
+            suggestions = difflib.get_close_matches(
+                item, keys, cutoff=0, n=100
+            )
+            return f'Invalid option {item!r}.\n' \
+                   f'Close matches: {suggestions!r}.'
+        else:
+            return super().__str__()
+
+
+class Dispatcher(dict):
+    """
+    Is basically a dict with a better error message on key error.
+    >>> d = Dispatcher(abc=1, bcd=2)
+    >>> d['acd']  #doctest: +ELLIPSIS
+    Traceback (most recent call last):
+    ...
+    mapping.DispatchError: Invalid option 'acd'.
+    Close matches: ['bcd', 'abc'].
+    """
+
     def __getitem__(self, item):
         try:
             return super().__getitem__(item)
-        except KeyError:
-            raise KeyError(
-                f'Invalid option {item!r}. Possible keys are {self.keys()!r}.'
-            )
+        except KeyError as e:
+            raise DispatchError(item, self.keys()) from e
