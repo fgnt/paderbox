ba2f12fb707146a04fcb06a47c6e7d2dee2b351c
Lukas Drude <mail@lukas-drude.de>
2018-12-20 13:53:28 +0100

    Add more to notebook.py, remove some mpl tricks from CBJ

--- paderbox/visualization/__init__.py
+++ paderbox/visualization/__init__.py
@@ -5,6 +5,7 @@ from . import plot
 
 from .context_manager import LatexContextManager
 from .context_manager import figure_context
+from .context_manager import axes_context
 from .module_facet_grid import facet_grid
 from .module_facet_grid import facet_grid_zero_space_time_frequency_plot
 from .plot import time_series
