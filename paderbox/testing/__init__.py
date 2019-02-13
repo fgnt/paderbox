6159a5311870a13c147686074d8b96369c317e04
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2019-02-13 17:29:44 +0100

    add missing assert_doctest_like_equal in init

--- paderbox/testing/__init__.py
+++ paderbox/testing/__init__.py
@@ -5,4 +5,5 @@ assertions for our cases.
 from paderbox.testing.module_asserts import *
 from numpy.testing import *
 from paderbox.testing.condition import retry
+from paderbox.testing.doctest_compare import assert_doctest_like_equal
 import paderbox.testing.attr
