8a72205f2197cfbc4e87de1cde432b68e250c251
jensheit <heitkaemper@nt.upb.de>
2019-03-27 15:45:58 +0100

    change kaldi.bash to allow input of 1 to switch to kaldi build in progress

--- bash/kaldi.bash
+++ bash/kaldi.bash
@@ -1,6 +1,13 @@
 # This code is intended to be used in all test cases which require Kaldi.
 # This file can be sourced in your own .bashrc or .env file.
-if [[ $(hostname) =~ ^ntsim.*|ntpc9|ntjenkins ]];
-then
-    export KALDI_ROOT=/net/vol/jenkins/kaldi/2018-08-08_10-09-05_8e97639b7066b5d8a649827975263b6fc288e6ef
+JENKINS_KALDI_ROOT=/net/vol/jenkins/kaldi/
+CURRENT_WORKING_KALDI_ROOT=$JENKINS_KALDI_ROOT/2018-08-08_10-09-05_8e97639b7066b5d8a649827975263b6fc288e6ef
+CURRENT_BUILDING_KALDI_ROOT=$JENKINS_KALDI_ROOT/2018-08-08_10-09-05_8e97639b7066b5d8a649827975263b6fc288e6ef
+
+if [[ $(hostname) =~ ^ntsim.*|ntpc9|ntjenkins & $1 ]]; then
+    if [ $# -eq 0 ]; then
+            export KALDI_ROOT=CURRENT_WORKING_KALDI_ROOT
+    else
+        if [ $1 -eq 1 ]; then
+            export KALDI_ROOT=CURRENT_BUILDING_KALDI_ROOT
 fi
