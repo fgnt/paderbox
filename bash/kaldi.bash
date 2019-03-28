90330384956ca121d3c364435ae3e3fb54639849
jensheit <heitkaemper@nt.upb.de>
2019-03-28 09:48:18 +0100

    fixed the comments

--- bash/kaldi.bash
+++ bash/kaldi.bash
@@ -1,10 +1,9 @@
 # This code is intended to be used in all test cases which require Kaldi.
-# This file can be sourced in your own .bashrc or .env file.
 JENKINS_KALDI_ROOT=/net/vol/jenkins/kaldi/
 CURRENT_WORKING_KALDI_ROOT=$JENKINS_KALDI_ROOT/2018-08-08_10-09-05_8e97639b7066b5d8a649827975263b6fc288e6ef
 CURRENT_BUILDING_KALDI_ROOT=$JENKINS_KALDI_ROOT/2018-08-08_10-09-05_8e97639b7066b5d8a649827975263b6fc288e6ef
 
-if [[ $(hostname) =~ ^ntsim.*|ntpc9|ntjenkins ]]; then
+if [[ $(hostname) =~ ^ntjenkins ]]; then
     if [ $# -eq 0 ]; then
             export KALDI_ROOT=$CURRENT_WORKING_KALDI_ROOT
     else
