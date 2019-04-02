373ee43c3add576a41e99aeaf62c54ab733d40a8
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2019-04-02 10:30:59 +0200

    fix jenkins_common.bash to be sourced from other repo

--- jenkins_common.bash
+++ jenkins_common.bash
@@ -2,11 +2,6 @@
 
 renice -n 20 $$
 
-# paths
-TOOLBOX=$(readlink -f ./toolbox)
-
-source /net/software/python/2018_12/anaconda/bin/activate
-
 # set a prefix for each cmd
 green='\033[0;32m'
 NC='\033[0m' # No Color
@@ -15,18 +10,25 @@ trap 'echo -e "${green}$ $BASH_COMMAND ${NC}"' DEBUG
 # Force Exit 0
 trap 'exit 0' EXIT SIGINT SIGTERM
 
+source /net/software/python/2018_12/anaconda/bin/activate
+
 # Use a pseudo virtualenv, http://stackoverflow.com/questions/2915471/install-a-python-package-into-a-different-directory-using-pip
 mkdir -p venv
 export PYTHONUSERBASE=$(readlink -m venv)
+
+# paths
+# TOOLBOX=$(readlink -f ./toolbox)
+TOOLBOX="$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
 
 # Refresh files...
 ls /net/ssd/software/conda/lib/python3.6/lib-dynload/../../ > /dev/null
 
 # adds a KALDI_ROOT
-source "`dirname "$0"`/bash/kaldi.bash"
+source "${TOOLBOX}/bash/kaldi.bash"
 
 # Refresh toolbox
 pip uninstall --quiet --yes paderbox
 pip show paderbox
 pip install --quiet --user -e ${TOOLBOX}
 pip show paderbox
+
