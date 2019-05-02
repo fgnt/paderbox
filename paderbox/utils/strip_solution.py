3f67d053ea562e4f3b768622266b11f71ca92b6f
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2019-05-02 13:21:17 +0200

    strip_solution: use nbformat

--- paderbox/utils/strip_solution.py
+++ paderbox/utils/strip_solution.py
@@ -4,39 +4,74 @@ $ python -m paderbox.utils.strip_solution name_template.ipynb name_solution.ipyn
 or directly with
 $ paderbox.strip_solution name_template.ipynb name_solution.ipynb
 """
-from pathlib import Path
 import re
+from pathlib import Path
+
 import fire
-
-class FileExistsWarning(ResourceWarning):
-    pass
+import nbformat
 
 
-REPLACE_PATTERN = re.compile(r'^(\s*)"(?:(\s*).*\s{0,2})?# (\d*)\s?REPLACE (.*(?:\\n)?",?)$')
-COMMENT_PATTERN = re.compile(r'^(\s*)"(\s*)(.*)\s+# COMMENT\s*((?:\\n)?",?)$')
-TRAILING_COMMA_PATTERN = re.compile(r',\\n(\s*)]')
-TRAILING_COMMA_SUBST = r'\\n\1]'
+CODE_MAGIC_WORD = '# REPLACE'
+LATEX_MAGIC_WORD = '% REPLACE'
 
 
-def remove_trailing_commas(json_like):
-    """Remove trailing commas from *json_like* and return the result.
-
-    Source: https://gist.github.com/liftoff/ee7b81659673eca23cd9fc0d8b8e68b7
-    Example::
-        >>> remove_trailing_commas('{"foo":"bar","baz":["blah",],}')
-        '{"foo":"bar","baz":["blah"]}'
+def code_replace(source, cell_type='code'):
     """
-    trailing_object_commas_re = re.compile(
-        r'(,)\s*}(?=([^"\\]*(\\.|"([^"\\]*\\.)*[^"\\]*"))*[^"]*$)')
-    trailing_array_commas_re = re.compile(
-        r'(,)\s*\](?=([^"\\]*(\\.|"([^"\\]*\\.)*[^"\\]*"))*[^"]*$)')
-    # Fix objects {} first
-    objects_fixed = trailing_object_commas_re.sub("}", json_like)
-    # Now fix arrays/lists [] and return the result
-    return trailing_array_commas_re.sub("]", objects_fixed)
+
+    Args:
+        source: solutio code
+        cell_type: 'code', 'markdown' or 'raw'
+
+    Returns:
+        template code
+
+    >>> code_replace('return 1. / (1 + np.exp(-x))  # REPLACE return ???')
+    'return ???'
+    >>> print(code_replace(
+    ... '''b = 1
+    ... a = 1. / (1 + np.exp(-x))  # REPLACE
+    ... return 1. / (1 + np.exp(-x))  # REPLACE return ???'''
+    ... ))
+    b=1
+    return ???
+    >>> print(code_replace(
+    ... '''b = 1
+    ... a = 1. / (1 + np.exp(-x))  # REPLACE a =
+    ... return 1. / (1 + np.exp(-x))  # REPLACE return ???'''
+    ... ))
+    b = 1
+    a =
+    return ???
+
+    """
+    if cell_type in ['code', 'markdown']:
+        MAGIC_WORD = {
+            'code': CODE_MAGIC_WORD,
+            'markdown': LATEX_MAGIC_WORD,
+        }[cell_type]
+
+        if MAGIC_WORD in source:
+            new_source_lines = []
+            for line in source.split('\n'):
+                if MAGIC_WORD in line:
+                    solution, template = line.split(MAGIC_WORD)
+                    # Remove leading whitespaces
+                    template = template.lstrip(' ')
+                    if template == '':
+                        continue
+                    whitespace = re.search('( *)', line).group(1)
+                    new_source_lines.append(whitespace + template.lstrip(' '))
+                else:
+                    new_source_lines.append(line)
+            source = '\n'.join(new_source_lines)
+    elif cell_type in ['raw']:
+        pass
+    else:
+        raise TypeError(cell_type, source)
+    return source
 
 
-def replace(old_path, new_path):
+def nb_replace(old_path, new_path):
     """Remove the solution from a Jupyter notebook.
 
     For example, assume the following line is in a notebook:
@@ -46,10 +81,10 @@ def replace(old_path, new_path):
         x = ???
 
     The following example
-        y = 42  # 3 REPLACE y = # TODO
-        q = 328
-        z = y * q
-    will result in
+        y = 42  # REPLACE y = # TODO
+        q = 328 # REPLACE
+        z = y * q # REPLACE
+    will result in (Without an replacement the line will be deleted)
         y = # TODO
 
     And the example
@@ -60,62 +95,28 @@ def replace(old_path, new_path):
     old_path = Path(old_path)
     new_path = Path(new_path)
 
+    assert old_path != new_path, (old_path, new_path)
+    assert old_path.is_file(), f'{old_path} is not a file.'
     assert old_path.name.endswith('_solution.ipynb'), old_path
     assert new_path.name.endswith('_template.ipynb'), new_path
-    assert old_path.is_file(), f'{old_path} is not a file.'
     assert not new_path.is_file(), f'{new_path} already exists.'
 
+    nb = nbformat.read(str(old_path), nbformat.NO_CONVERT)
+
     replacements = 0
-
-    with old_path.open() as old, new_path.open('w') as new:
-        replace_lines_count = 0
-        for line in old:
-            replace_match = REPLACE_PATTERN.match(line)
-            comment_match = COMMENT_PATTERN.match(line)
-            if replace_match is not None:
-                outer_whitespace = replace_match.group(1)
-                if replace_match.group(2) is not None:
-                    inner_whitespace = replace_match.group(2)
-                else:
-                    inner_whitespace = ''
-                # replace_lines_count: '# n REPLACE ...' lets the script remove
-                # the n-1 following lines. Useful for multi-line solutions.
-                if replace_match.group(3):
-                    replace_lines_count = int(replace_match.group(3)) - 1
-                else:
-                    replace_lines_count = 0
-                template_and_ending = replace_match.group(4)
-                new.write(outer_whitespace + '"' + inner_whitespace
-                          + template_and_ending + '\n')
-                replacements += 1
-            elif comment_match is not None:
-                outer_whitespace = comment_match.group(1)
-                inner_whitespace = comment_match.group(2)
-                content = comment_match.group(3)
-                ending = comment_match.group(4)
-                new.write(outer_whitespace + '"' + inner_whitespace
-                          + '# ' + content + ending + '\n')
-            else:
-                if replace_lines_count == 0:
-                    new.write(line)
-                else:
-                    replace_lines_count -= 1
-                    replacements += 1
-
-    # remove obsolete trailing commas. This can happen if a REPLACE line
-    # leads to replacing the last line of a cell.
-
-    with new_path.open('r') as new:
-        content = remove_trailing_commas(new.read())
-    with new_path.open('w') as new:
-        new.write(content)
+    for i, cell in enumerate(nb['cells']):
+        cell_source = code_replace(cell['source'], cell['cell_type'])
+        if cell_source != cell['source']:
+            replacements += 1
+            cell['source'] = cell_source
 
     print(f'Replaced {replacements} lines of code.')
+    nbformat.write(nb, new_path, nbformat.NO_CONVERT)
 
 
 def entry_point():
-    fire.Fire(replace)
+    fire.Fire(nb_replace)
 
 
 if __name__ == '__main__':
-    fire.Fire(replace)
+    fire.Fire(nb_replace)
