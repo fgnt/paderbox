66b5e905789cfed0414e3c80de5caf461d43b15a
Lukas Drude <mail@lukas-drude.de>
2019-12-04 12:43:17 +0100

    Update phase feature docstrings

--- paderbox/transform/module_phase_features.py
+++ paderbox/transform/module_phase_features.py
@@ -5,6 +5,18 @@ import paderbox.math.directional as directional
 
 
 def transform_to_baseband(X, size, shift):
+    """Assumes linear frequency dependency.
+
+    Then phase is more consistent over frequencies.
+
+    Args:
+        X:
+        size:
+        shift:
+
+    Returns:
+
+    """
     X = deepcopy(X)
     T, _, F = X.shape
     for t in range(T):
@@ -14,6 +26,20 @@ def transform_to_baseband(X, size, shift):
 
 
 def get_phase_features(X, size, shift):
+    """Experimental phase features for SPP estimation.
+
+    These experimental features were originally used because the phase
+    differences, at least when small STFT shifts are used, correspond better
+    to SPP than the phase itself.
+
+    Args:
+        X:
+        size:
+        shift:
+
+    Returns:
+
+    """
     X_base = transform_to_baseband(X, size, shift)
     phase = np.angle(X_base)
     delta = np.zeros_like(phase)
