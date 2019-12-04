5dd28602acc2f5a17eaeb42dd506f053387ccaad
Thomas Glarner <tglarner@mail.upb.de>
2019-12-04 12:32:35 +0100

    Remove normalize code from transform __init__
    This code is part of module_normalize now.

--- paderbox/transform/__init__.py
+++ paderbox/transform/__init__.py
@@ -11,12 +11,6 @@ from .module_stft import (
     get_stft_center_frequencies,
 )
 
-import numpy as np
-from paderbox.transform.module_resample import resample_sox
-
-
-def normalize_mean_variance(data, axis=0, eps=1e-6):
-    """ Normalize features.
 from .module_filter import (
     preemphasis,
     inverse_preemphasis,
@@ -24,12 +18,6 @@ from .module_filter import (
     preemphasis_with_offset_compensation,
 )
 
-    :param data: Any feature
-    :param axis: Time dimensions, default is 0
-    :return: Normalized observation
-    """
-    return (data - np.mean(data, axis=axis, keepdims=True)) /\
-        (np.std(data, axis=axis, keepdims=True) + eps)
 from .module_fbank import fbank, logfbank
 from .module_mfcc import mfcc, mfcc_velocity_acceleration
 from .module_normalize import normalize_mean_variance
