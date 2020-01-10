52f39b5c7ebb3877b02455e794890c6790f498a7
Janek Ebbers <ebbers@nt.upb.de>
2020-01-10 17:29:28 +0100

    merge review

--- paderbox/io/download.py
+++ paderbox/io/download.py
@@ -10,18 +10,18 @@ from urllib.request import urlretrieve
 from tqdm import tqdm
 
 
-def download_file(remote_file, local_file):
+def download_file(remote_file, local_file, exist_ok=False):
     """
     Download single file to local_dir
 
     Args:
         remote_file:
         local_file:
+        exist_ok:
 
     Returns:
 
     """
-    local_dir = path.dirname(local_file)
     if not path.isfile(local_file):
         def progress_hook(t):
             """
@@ -30,7 +30,7 @@ def download_file(remote_file, local_file):
             `with` syntax).
             """
 
-            last_b = [0]
+            last_b = 0
 
             def inner(b=1, bsize=1, tsize=None):
                 """
@@ -42,17 +42,19 @@ def download_file(remote_file, local_file):
                     Total size (in tqdm units). If [default: None]
                     remains unchanged.
                 """
+                nonlocal last_b
                 if tsize is not None:
                     t.total = tsize
-                t.update((b - last_b[0]) * bsize)
-                last_b[0] = b
+                t.update((b - last_b) * bsize)
+                last_b = b
 
             return inner
 
-        tmp_file = path.join(local_dir, 'tmp_file')
+        tmp_file = local_file + '_tmp_file'
         with tqdm(
                 desc="{0: >25s}".format(
-                    path.splitext(remote_file.split('/')[-1])[0]),
+                    path.splitext(remote_file.split('/')[-1])[0]
+                ),
                 file=sys.stdout,
                 unit='B',
                 unit_scale=True,
@@ -60,7 +62,6 @@ def download_file(remote_file, local_file):
                 leave=False,
                 ascii=True
         ) as t:
-
             urlretrieve(
                 remote_file,
                 filename=tmp_file,
@@ -68,20 +69,23 @@ def download_file(remote_file, local_file):
                 data=None
             )
         os.rename(tmp_file, local_file)
+    elif not exist_ok:
+        raise FileExistsError(local_file)
     return local_file
 
 
-def extract_file(local_file):
+def extract_file(local_file, exist_ok=False):
     """
-    extract files if local_file zip or tar.gz else do nothing
+    If local_file is .zip or .tar.gz files are extracted.
+    If local_file is .zip a common first level folder is omitted.
 
     Args:
         local_file:
+        exist_ok:
 
     Returns:
 
     """
-
     local_dir = path.dirname(local_file)
     if path.isfile(local_file):
 
@@ -93,24 +97,18 @@ def extract_file(local_file):
                 for name in z.namelist():
                     if not name.endswith('/'):
                         parts.append(name.split('/')[:-1])
-                prefix = path.commonprefix(parts) or ''
-
-                if prefix:
-                    if len(prefix) > 1:
-                        prefix_ = list()
-                        prefix_.append(prefix[0])
-                        prefix = prefix_
-
-                    prefix = '/'.join(prefix) + '/'
+                prefix = path.commonprefix(parts)
+                prefix = prefix[0] + '/' if prefix else ''
                 offset = len(prefix)
 
                 # Start extraction
                 members = z.infolist()
                 for i, member in enumerate(members):
-                    if len(member.filename) > offset:
+                    if member.filename != prefix:
                         member.filename = member.filename[offset:]
-                        if not path.isfile(path.join(
-                                local_dir, member.filename)):
+                        if not path.isfile(
+                                path.join(local_dir, member.filename)
+                        ):
                             try:
                                 z.extract(member=member, path=local_dir)
                             except KeyboardInterrupt:
@@ -122,26 +120,31 @@ def extract_file(local_file):
 
                                 # Quit
                                 sys.exit()
+                        elif not exist_ok:
+                            raise FileExistsError(path.join(local_dir, member.filename))
             os.remove(local_file)
 
         elif local_file.endswith('.tar.gz'):
-            tar = tarfile.open(local_file, "r:gz")
-            for i, tar_info in enumerate(tar):
-                if not path.isfile(
-                        path.join(local_dir, tar_info.name)):
-                    tar.extract(tar_info, local_dir)
-                tar.members = []
-            tar.close()
+            with tarfile.open(local_file, "r:gz") as tar:
+                for i, tar_info in enumerate(tar):
+                    if not path.isfile(
+                        path.join(local_dir, tar_info.name)
+                    ):
+                        tar.extract(tar_info, local_dir)
+                    elif not exist_ok:
+                        raise FileExistsError(path.join(local_dir, tar_info.name))
+                    tar.members = []
             os.remove(local_file)
 
 
-def download_file_list(file_list, target_dir, logger=None):
+def download_file_list(file_list, target_dir, exist_ok=False, logger=None):
     """
     Download file_list to target_dir
 
     Args:
         file_list:
         target_dir:
+        exist_ok:
         logger:
 
     Returns:
@@ -159,15 +162,21 @@ def download_file_list(file_list, target_dir, logger=None):
 
     local_files = list()
     for remote_file in item_progress:
-        local_files.append(download_file(
-            remote_file, path.join(target_dir, path.basename(remote_file))
-        ))
+        local_files.append(
+            download_file(
+                remote_file,
+                path.join(target_dir, path.basename(remote_file)),
+                exist_ok=exist_ok
+            )
+        )
 
-    item_progress = tqdm(local_files,
-                         desc="{0: <25s}".format('Extract files'),
-                         file=sys.stdout,
-                         leave=False,
-                         ascii=True)
+    item_progress = tqdm(
+        local_files,
+        desc="{0: <25s}".format('Extract files'),
+        file=sys.stdout,
+        leave=False,
+        ascii=True
+    )
 
     if logger is not None:
         logger.info('Starting Extraction')
@@ -180,5 +189,7 @@ def download_file_list(file_list, target_dir, logger=None):
                         title='Extract files ',
                         item_id=_id,
                         total=len(item_progress),
-                        package=local_file))
-            extract_file(local_file)
+                        package=local_file
+                    )
+                )
+            extract_file(local_file, exist_ok=exist_ok)
