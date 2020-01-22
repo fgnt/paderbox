98ca55c28875f3bbeb4497bab1573a348ad92d3d
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2020-01-22 14:17:43 +0100

    add test_yaml

---
+++ tests/io_tests/test_yaml.py
@@ -0,0 +1,24 @@
+import tempfile
+from pathlib import Path
+import paderbox as pb
+
+
+def test_yaml_load():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmpdir = Path(tmpdir)
+        path = (tmpdir / 'test.yaml')
+        path.write_text('a: 1\nb: 2')
+
+        assert pb.io.load_yaml(path) == {'a': 1, 'b': 2}
+        assert pb.io.load_yaml_unsafe(path) == {'a': 1, 'b': 2}
+
+
+def test_yaml_dump():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmpdir = Path(tmpdir)
+        path = (tmpdir / 'test.yaml')
+
+        data = {'a': 1, 'b': 2}
+
+        pb.io.dump_yaml(data, path)
+        assert path.read_text() == 'a: 1\nb: 2\n'
