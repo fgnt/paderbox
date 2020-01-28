2154dbd7ae6f0c9b695715d21cc0e2625ff6b92a
jensheit <heitkaemper@nt.upb.de>
2020-01-28 08:36:32 +0100

    updates the building kaldi root

--- bash/kaldi.bash
+++ bash/kaldi.bash
@@ -1,7 +1,7 @@
 # This code is intended to be used in all test cases which require Kaldi.
 JENKINS_KALDI_ROOT=/net/vol/jenkins/kaldi/
 CURRENT_WORKING_KALDI_ROOT=$JENKINS_KALDI_ROOT/2018-08-08_10-09-05_8e97639b7066b5d8a649827975263b6fc288e6ef
-CURRENT_BUILDING_KALDI_ROOT=$JENKINS_KALDI_ROOT/2018-08-08_10-09-05_8e97639b7066b5d8a649827975263b6fc288e6ef
+CURRENT_BUILDING_KALDI_ROOT=$JENKINS_KALDI_ROOT/2020-01-10_10-32-09_cbdbedefcdd47e02a685c1dc2d1b128c30bdf6b2
 
 if [[ $USER =~ ^jenkins ]]; then
     if [ $# -eq 0 ]; then
