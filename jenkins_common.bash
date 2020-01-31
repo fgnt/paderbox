2254a5c8a0e7f8a87741bd81144734632efd01a0
jensheit <heitkaemper@nt.upb.de>
2020-01-31 10:31:24 +0100

    updates test to use the test requirements

--- jenkins_common.bash
+++ jenkins_common.bash
@@ -28,6 +28,6 @@ source "${TOOLBOX}/bash/kaldi.bash"
 # Refresh toolbox
 pip uninstall --quiet --yes paderbox
 pip show paderbox
-pip install --quiet --user -e ${TOOLBOX}
+pip install --quiet --user -e ${TOOLBOX}[test]
 pip show paderbox
 
