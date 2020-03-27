72439f8d848e62512748c851f7fcd0a5ff5e3a0f
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2020-03-27 14:59:12 +0100

    ArrayIntervall: add ones and bool operators

--- paderbox/array/intervall/rttm.py
+++ paderbox/array/intervall/rttm.py
@@ -33,7 +33,7 @@ def _merge_dicts(*dicts):
         dicts = dicts[0]
     keys = [k for d in dicts for k in d.keys()]
     # Duplicate test
-    assert len(keys) == len(set(keys)), dicts
+    assert len(keys) == len(set(keys)), (keys, dicts)
 
     return {
         k: v
@@ -75,7 +75,7 @@ def from_rttm(rttm_file, shape=None, sample_rate=16000):
             from_rttm(f, **kwargs)
             for f in rttm_file
         ])
-
+    
     rttm_file = Path(rttm_file)
     return from_rttm_str(
         rttm_file.read_text(), **kwargs)
@@ -112,37 +112,57 @@ def from_rttm_str(rttm_str, shape=None, sample_rate=16000):
     return deflatten(data, sep=None)
 
 
-def to_rttm_str(data, smaple_rate=16000):
+def to_rttm_str(data, sample_rate=16000):
     """
+
+    data: nested dictionary:
+     - `data[file_id][speaker_id] = activity`
+     - `activity` is an boolian array that indicate activity or not on sample
+       resolution (e.g. ArrayIntervall or np.array)
+
+
     >>> ar1 = zeros(None)
     >>> ar1[0:16000] = 1
     >>> ar1[32000:48000] = 1
-    >>> ar2 = zeros(None)
+    >>> ar2 = np.zeros(shape=50000, dtype=np.bool)
     >>> ar2[0:32000] = 1
     >>> data = {'S02': {'1': ar1, '2': ar2}}
     >>> data
-    {'S02': {'1': ArrayIntervall("0:16000, 32000:48000", shape=None), '2': ArrayIntervall("0:32000", shape=None)}}
+    {'S02': {'1': ArrayIntervall("0:16000, 32000:48000", shape=None), '2': array([ True,  True,  True, ..., False, False, False])}}
     >>> print(to_rttm_str(data))
     SPEAKER S02 1 0 1 <NA> <NA> 1 <NA>
     SPEAKER S02 1 2 1 <NA> <NA> 1 <NA>
     SPEAKER S02 1 0 2 <NA> <NA> 2 <NA>
-    >>> print(to_rttm_str(data, smaple_rate=320_000))
+    >>> print(to_rttm_str(data, sample_rate=320_000))
     SPEAKER S02 1 0 0.05 <NA> <NA> 1 <NA>
     SPEAKER S02 1 0.1 0.05 <NA> <NA> 1 <NA>
     SPEAKER S02 1 0 0.1 <NA> <NA> 2 <NA>
+    >>> print(to_rttm_str({'S02': [ar1, ar2]}))
+    SPEAKER S02 1 0 1 <NA> <NA> 0 <NA>
+    SPEAKER S02 1 2 1 <NA> <NA> 0 <NA>
+    SPEAKER S02 1 0 2 <NA> <NA> 1 <NA>
 
     """
     lines = []
     for file_id in data.keys():
-        for name in data[file_id].keys():
+        if isinstance(data[file_id], dict):
+            keys = data[file_id].keys()
+        else:
+            keys = range(len(data[file_id]))
+
+        for name in keys:
             content = data[file_id][name]
             if isinstance(content, np.ndarray):
                 content = ArrayIntervall(content)
             for begin, end in content.intervals:
-                duration = decimal.Decimal(end - begin) / smaple_rate
-                begin = decimal.Decimal(begin) / smaple_rate
+                duration = decimal.Decimal(int(end - begin)) / sample_rate
+                begin = decimal.Decimal(int(begin)) / sample_rate
                 lines.append(
                     f'SPEAKER {file_id} 1 {begin} {duration} <NA> <NA> {name} <NA>'
                 )
 
     return '\n'.join(lines)
+
+
+def to_rttm(data, rttm_file, sample_rate=16000):
+    Path(rttm_file).write_text(to_rttm_str(data, sample_rate=sample_rate))
