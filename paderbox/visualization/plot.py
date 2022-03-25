f2a1e17fa1c8af5f1115632437f3e2b9a8abd312
Alexander Werning <awerning@mail.upb.de>
2022-03-25 11:23:25 +0100

    Change collections imports to collections.abc

--- paderbox/visualization/plot.py
+++ paderbox/visualization/plot.py
@@ -1,4 +1,5 @@
-from collections import OrderedDict, Iterable
+from collections import OrderedDict
+from collections.abc import Iterable
 from functools import wraps
 import itertools
 from warnings import warn
