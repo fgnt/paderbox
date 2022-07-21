797e733c812bd4baa2e574cf133f8157b2545e44
jensheit <heitkaemper@nt.upb.de>
2022-07-21 15:11:09 +0200

    update path to kaldi for internal tests

--- bash/kaldi.bash
+++ bash/kaldi.bash
@@ -3,7 +3,7 @@ JENKINS_KALDI_ROOT=/net/vol/jenkins/kaldi/
 CURRENT_WORKING_KALDI_ROOT=$JENKINS_KALDI_ROOT/2018-08-08_10-09-05_8e97639b7066b5d8a649827975263b6fc288e6ef
 CURRENT_BUILDING_KALDI_ROOT=$JENKINS_KALDI_ROOT/2020-09-23_09-43-55_5e6f08c13efc8ab425b77debcb33bae13dc6b31e
 
-if [[ $USER =~ ^jenkins ]]; then
+if [[ $(hostname) =~ ntjenkins ]]; then
     if [ $# -eq 0 ]; then
             export KALDI_ROOT=$CURRENT_WORKING_KALDI_ROOT
     else
