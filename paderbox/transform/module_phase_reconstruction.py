010924946339c6ad559604a0d53b6ee283982a14
Michael Kuhlmann <kuhlmann@nt.uni-paderborn.de>
2023-07-06 10:24:14 +0200

    Add Griffin-Lim subroutine step

--- paderbox/transform/module_phase_reconstruction.py
+++ paderbox/transform/module_phase_reconstruction.py
@@ -2,6 +2,21 @@ import numpy as np
 from paderbox.transform.module_stft import STFT
 
 
+def _griffin_lim_step(
+    x: np.ndarray,
+    reconstruction_stft: np.ndarray,
+    stft: STFT
+):
+    reconstruction_angle = np.angle(reconstruction_stft)
+    # Discard magnitude part of the reconstruction and use the supplied
+    # magnitude spectrogram instead.
+    proposal_spec = x * np.exp(1.0j * reconstruction_angle)
+    audio = stft.inverse(proposal_spec)
+    reconstruction_stft = stft(audio)
+
+    return reconstruction_stft, audio
+
+
 def griffin_lim(x, stft: STFT, iterations=100, verbose=False):
     """
     Reconstructs phase from magnitudes using Griffin-Lim algorithm and returns
@@ -28,13 +43,11 @@ def griffin_lim(x, stft: STFT, iterations=100, verbose=False):
     nsamples = int(stft.frames_to_samples(nframes))
     # Initialize the reconstructed signal.
     audio = np.random.randn(nsamples)
+    reconstruction_stft = stft(audio)
     for n in range(iterations):
-        reconstruction_stft = stft(audio)
-        reconstruction_angle = np.angle(reconstruction_stft)
-        # Discard magnitude part of the reconstruction and use the supplied
-        # magnitude spectrogram instead.
-        proposal_spec = x * np.exp(1.0j * reconstruction_angle)
-        audio = stft.inverse(proposal_spec)
+        reconstruction_stft, audio = _griffin_lim_step(
+            x, reconstruction_stft, stft
+        )
 
         if verbose:
             reconstruction_magnitude = np.abs(reconstruction_stft)
