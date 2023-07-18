2c86c4dbd9f7359495cd4289362b125b97ab3c8e
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2023-07-18 11:09:05 +0200

    pprint: Allow max_seq_length to be a list, to specify different max_seq_length for different depths

--- paderbox/utils/pretty.py
+++ paderbox/utils/pretty.py
@@ -53,6 +53,50 @@ class _MyRepresentationPrinter(IPython.lib.pretty.RepresentationPrinter):
         self.type_pprinters[type({}.items())] = \
             IPython.lib.pretty._seq_pprinter_factory('dict_items(', ')')
 
+    @property
+    def max_seq_length(self):
+        if isinstance(self._max_seq_length, int):
+            return self._max_seq_length
+        elif isinstance(self._max_seq_length, (tuple, list)):
+            if self.depth >= len(self._max_seq_length):
+                return self._max_seq_length[-1]
+            else:
+                return self._max_seq_length[self.depth]
+        return self._max_seq_length
+
+    @max_seq_length.setter
+    def max_seq_length(self, value):
+        self._max_seq_length = value
+
+    depth = -1
+
+    def _enumerate(self, seq):
+        """
+        >>> nested = {l: [tuple(range(3))]*4 for l in 'abcd'}
+        >>> pprint(nested)
+        {'a': [(0, 1, 2), (0, 1, 2), (0, 1, 2), (0, 1, 2)],
+         'b': [(0, 1, 2), (0, 1, 2), (0, 1, 2), (0, 1, 2)],
+         'c': [(0, 1, 2), (0, 1, 2), (0, 1, 2), (0, 1, 2)],
+         'd': [(0, 1, 2), (0, 1, 2), (0, 1, 2), (0, 1, 2)]}
+        >>> pprint(nested, max_seq_length=[3, 2])
+        {'a': [(0, 1, ...), (0, 1, ...), ...],
+         'b': [(0, 1, ...), (0, 1, ...), ...],
+         'c': [(0, 1, ...), (0, 1, ...), ...],
+         ...}
+        >>> pprint(nested, max_seq_length=[3, 2, 1])
+        {'a': [(0, ...), (0, ...), ...],
+         'b': [(0, ...), (0, ...), ...],
+         'c': [(0, ...), (0, ...), ...],
+         ...}
+        >>> pprint(nested, max_seq_length=2)
+        {'a': [(0, 1, ...), (0, 1, ...), ...],
+         'b': [(0, 1, ...), (0, 1, ...), ...],
+         ...}
+        """
+        self.depth += 1
+        yield from super()._enumerate(seq)
+        self.depth -= 1
+
 
 def pprint(
         obj,
@@ -69,6 +113,8 @@ def pprint(
     Differences:
      - Shortens the __repr__ of large np.ndarray and torch.Tensor
      - Support multiple objects (Causes bad readable error in original)
+     - Support list/tuple for max_seq_length, where
+       max(depth, len(max_seq_length)-1) is used as index.
 
 
     >>> pprint([np.array([1.]), np.array([1.]*100)])
