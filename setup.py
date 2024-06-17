c9fa6da51b6f14f4618e5390b228e099c64fbcbd
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2024-06-17 10:51:28 +0000

    Bump version: 0.0.6 → 0.0.7

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.6',
+    version='0.0.7',
 
     description='Collection of utilities in the department of communications engineering of the UPB',
     long_description=long_description,
